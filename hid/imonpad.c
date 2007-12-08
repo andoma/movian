057da7584b3906843626497289e728d6650862ef
Andreas Öman <andreas@lonelycoder.com>
2007-12-08 11:51:16 +0000

    Revamp the pad to key translation.

--- hid/imonpad.c
+++ hid/imonpad.c
@@ -39,6 +39,7 @@ static const struct {
   uint32_t code;
   input_key_t key;
 } imonpadmap[] = {
+  { 0x288195b7, INPUT_KEY_QUIT },
   { 0x28A395B7, INPUT_KEY_VOLUME_UP },
   { 0x28A595B7, INPUT_KEY_VOLUME_DOWN },
   { 0x2B9595B7, INPUT_KEY_VOLUME_MUTE },
@@ -71,6 +72,27 @@ static const struct {
   { 0x2a9315b7, INPUT_KEY_SELECT },
 };
 
+static int repeat_rate;
+static int repeat_rate0;
+
+#define REPEAT_RATE_SLOWEST 1000000
+
+static void *
+imonpad_compute_repeat_rate(void *aux)
+{
+  repeat_rate = repeat_rate0 = REPEAT_RATE_SLOWEST;
+
+  while(1) {
+    usleep(100000);
+    repeat_rate = (repeat_rate * 7 + repeat_rate0) / 8;
+  }
+}
+
+
+
+
+
+
 static void *
 imonpad_thread(void *aux)
 {
@@ -80,8 +102,9 @@ imonpad_thread(void *aux)
   int dx, dy;
   inputevent_t ie;
   float angle;
-  int repeat_thres = 0;
-  int64_t down_nav_ts = 0, last_nav_ts = 0, ts;
+  int64_t last_nav_generated_ts = 0, ts, delta;
+  int64_t last_nav_sens_ts = 0;
+  int k;
 
   while(1) {
     if(fd == -1) {
@@ -115,55 +138,97 @@ imonpad_thread(void *aux)
       ie.type = INPUT_PAD;
       input_root_event(&ie);
 
-      /* */
+      /* Compute angle and vector length */
 
-      ts = showtime_get_ts();
+      if(dx < 0) {
+	angle = atanf((float)dy / (float)dx);
+      } else if(dx == 0) {
+	angle = (dy > 0 ? M_PI : 0) + M_PI * 0.5;
+      } else {
+	angle = M_PI + atanf((float)dy / (float)dx);
+      }
+      angle *= 360.0f / (M_PI * 2);
+      if(angle < 0)
+	angle = angle + 360;
 
-      if(ts - last_nav_ts > 100000)
-	down_nav_ts = ts;
+      /* Discard too light pressing on the pad,
+	 need to do this after the angle has been computed though*/
 
-      last_nav_ts = ts;
-      ts = last_nav_ts - down_nav_ts;
-
-      if(ts == 0 || ts > 250000) {
-
-	if(dx < 0) {
-	  angle = atanf((float)dy / (float)dx);
-	} else if(dx == 0) {
-	    angle = (dy > 0 ? M_PI : 0) + M_PI * 0.5;
-	} else {
-	  angle = M_PI + atanf((float)dy / (float)dx);
-	}
-	angle *= 360.0f / (M_PI * 2);
-	if(angle < 0)
-	  angle = angle + 360;
-
-	l = sqrt(dx * dx + dy * dy);
-
-	if(ts == 0 && l < 2)
-	  continue;
-	else if(ts != 0) {
-	  repeat_thres += l;
-	  if(repeat_thres < 10)
-	    continue;
-	  repeat_thres -= 10;
-	}
-
-#define ALIMIT 30
-
-	if(angle > 360 - ALIMIT || angle < 0 + ALIMIT)
-	  input_key_down(INPUT_KEY_LEFT);
-
-	if(angle > 90 - ALIMIT && angle < 90 + ALIMIT)
-	  input_key_down(INPUT_KEY_UP);
-	
-	if(angle > 180 - ALIMIT && angle < 180 + ALIMIT)
-	  input_key_down(INPUT_KEY_RIGHT);
-		
-	if(angle > 270 - ALIMIT && angle < 270 + ALIMIT)
-	  input_key_down(INPUT_KEY_DOWN);
+#define SENS 6
+      switch(dx) {
+      case -100 ... -SENS - 1:
+	dx += SENS;
+	break;
+      case -SENS ... SENS:
+	dx = 0;
+	break;
+      case SENS + 1 ... 100:
+	dx -= SENS;
+	break;
       }
 
+      switch(dy) {
+      case -100 ... -SENS - 1:
+	dy += SENS;
+	break;
+      case -SENS ... SENS:
+	dy = 0;
+	break;
+      case SENS + 1 ... 100:
+	dy -= SENS;
+	break;
+      }
+
+      l = sqrt(dx * dx + dy * dy);
+
+      /* If zero (user might still touch the pad though)
+	 bail out (ie. act as if nothing happens) */
+
+      if(l == 0)
+	continue;
+
+      /* Now, the angle will tell us which input event to generate
+	 and the vector length will be the repeat rate threshold */
+
+      /* We feed the repeat rate thru a IIR filter */
+
+      l = REPEAT_RATE_SLOWEST - l * 300000;
+      if(l < 50000)
+	l = 50000;
+
+      repeat_rate0 = l;
+
+      ts = showtime_get_ts();
+      delta = ts - last_nav_sens_ts;
+      last_nav_sens_ts = ts;
+
+      if(delta > 300000) {
+	/* No activity sensed, reset repeat */
+	repeat_rate0 = repeat_rate = REPEAT_RATE_SLOWEST;
+      }
+
+      delta = ts - last_nav_generated_ts;
+
+#define ALIMIT 30
+      k = 0;
+
+      if(angle > 360 - ALIMIT || angle < 0 + ALIMIT)
+	k = INPUT_KEY_LEFT;
+      
+      if(angle > 90 - ALIMIT && angle < 90 + ALIMIT)
+	k = INPUT_KEY_UP;
+      
+      if(angle > 180 - ALIMIT && angle < 180 + ALIMIT)
+	k = INPUT_KEY_RIGHT;
+      
+      if(angle > 270 - ALIMIT && angle < 270 + ALIMIT)
+	k = INPUT_KEY_DOWN;
+
+      if(k && delta > repeat_rate0) {
+	last_nav_generated_ts = ts;
+	input_key_down(k);
+	delta -= repeat_rate0;
+      }
       continue;
     }
 
@@ -187,4 +252,5 @@ imonpad_init(void)
 
   has_analogue_pad = 1;
   pthread_create(&ptid, NULL, imonpad_thread, NULL);
+  pthread_create(&ptid, NULL, imonpad_compute_repeat_rate, NULL);
 }
