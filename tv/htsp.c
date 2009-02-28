930be10b43a964c3bc68020dd86f9dd98d1f4f58
Andreas Öman <andreas@lonelycoder.com>
2009-02-28 16:30:37 +0000

    rename 'setAsync' -> 'enableAsyncMetadata'

--- tv/htsp.c
+++ tv/htsp.c
@@ -542,7 +542,7 @@ htsp_thread(void *aux)
 
     m = htsmsg_create();
 
-    htsmsg_add_str(m, "method", "setAsync");
+    htsmsg_add_str(m, "method", "enableAsyncMetadata");
     m = htsp_reqreply(hc, m, 0);
     if(m == NULL) {
       return NULL;
