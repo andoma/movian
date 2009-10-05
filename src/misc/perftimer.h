f33772e7c08ad5732e6d56e0c68459009659aa49
Andreas Öman <andreas@lonelycoder.com>
2009-10-05 21:24:20 +0000

    Add helper code for measuring delta time

---
+++ src/misc/perftimer.h
@@ -0,0 +1,39 @@
+#ifndef PERFTIMER_H__
+#define PERFTIMER_H__
+
+#include "showtime.h"
+
+typedef struct perftimer {
+  int avg;
+  int peak;
+  int64_t start;
+  int logsec;
+} perftimer_t;
+
+static inline void perftimer_start(perftimer_t *pt)
+{
+  pt->start = showtime_get_ts();
+}
+
+static inline void perftimer_stop(perftimer_t *pt, const char *str)
+{
+  int64_t now = showtime_get_ts();
+  int d = now - pt->start;
+  int s;
+
+  if(pt->avg == 0) {
+    pt->avg = d;
+  } else {
+    pt->avg = (d + pt->avg * 15) / 16;
+  }
+  
+  if(d > pt->peak)
+    pt->peak = d;
+
+  s = now / 1000000;
+
+  TRACE(TRACE_DEBUG, "timer", "%s: avg:%d, peak:%d",
+	str, pt->avg, pt->peak);
+}
+
+#endif /* PERFTIMER_H__ */
