6e4faab6d870c9fb8f8ebdecbe067f4b691246e3
Andreas Öman <andreas@lonelycoder.com>
2009-12-14 19:51:38 +0000

    Add HTTP connection reuse and pooling

--- src/fileaccess/fa_http.c
+++ src/fileaccess/fa_http.c
@@ -38,23 +38,126 @@
 
 extern char *htsversion;
 
+TAILQ_HEAD(http_connection_queue , http_connection);
+
+static struct http_connection_queue http_connections;
+static int http_parked_connections;
+static hts_mutex_t http_connections_mutex;
+
+typedef struct http_connection {
+  char hc_hostname[128];
+  int hc_port;
+
+  int hc_fd;
+
+  htsbuf_queue_t hc_spill;
+
+  TAILQ_ENTRY(http_connection) hc_link;
+
+  int hc_reused;
+
+} http_connection_t;
+
+
+/**
+ *
+ */
+static http_connection_t *
+http_connection_get(const char *hostname, int port, char *errbuf, int errlen)
+{
+  http_connection_t *hc;
+  int fd;
+
+  hts_mutex_lock(&http_connections_mutex);
+
+  TAILQ_FOREACH(hc, &http_connections, hc_link) {
+    if(!strcmp(hc->hc_hostname, hostname) && hc->hc_port == port) {
+      TAILQ_REMOVE(&http_connections, hc, hc_link);
+      http_parked_connections--;
+      hts_mutex_unlock(&http_connections_mutex);
+      hc->hc_reused = 1;
+      return hc;
+    }
+  }
+
+  hts_mutex_unlock(&http_connections_mutex);
+
+  
+  if((fd = tcp_connect(hostname, port, errbuf, errlen, 3000)) < 0)
+    return NULL;
+
+  hc = malloc(sizeof(http_connection_t));
+  snprintf(hc->hc_hostname, sizeof(hc->hc_hostname), "%s", hostname);
+  hc->hc_port = port;
+  hc->hc_fd = fd;
+  htsbuf_queue_init(&hc->hc_spill, 0);
+  hc->hc_reused = 0;
+  return hc;
+}
+
+
+/**
+ *
+ */
+static void
+http_connection_destroy(http_connection_t *hc)
+{
+  tcp_close(hc->hc_fd);
+  htsbuf_queue_flush(&hc->hc_spill);
+  free(hc);
+}
+
+
+/**
+ *
+ */
+static void
+http_connection_park(http_connection_t *hc)
+{
+  hts_mutex_lock(&http_connections_mutex);
+  TAILQ_INSERT_TAIL(&http_connections, hc, hc_link);
+
+  if(http_parked_connections == 5) {
+    hc = TAILQ_FIRST(&http_connections);
+    TAILQ_REMOVE(&http_connections, hc, hc_link);
+    http_connection_destroy(hc);
+  } else {
+    http_parked_connections++;
+  }
+
+  hts_mutex_unlock(&http_connections_mutex);
+}
+
+
+/**
+ *
+ */
+static void
+http_init(void)
+{
+  TAILQ_INIT(&http_connections);
+  hts_mutex_init(&http_connections_mutex);
+}
+
+
+/**
+ *
+ */
 typedef struct http_file {
   fa_handle_t h;
+
+  http_connection_t *hf_connection;
 
   char *hf_url;
   char *hf_auth;
   char *hf_location;
   char *hf_auth_realm;
 
-  int hf_fd;
-
-  htsbuf_queue_t hf_spill;
 
   char hf_line[1024];
 
   char hf_authurl[128];
   char hf_path[256];
-  char hf_hostname[128];
 
   int hf_chunked_transfer;
 
@@ -65,7 +168,6 @@ typedef struct http_file {
   int64_t hf_pos;
 
   int hf_isdir;
-  int hf_port;
 
   int hf_auth_failed;
   
@@ -77,6 +179,8 @@ typedef struct http_file {
   } hf_connection_mode;
 
 } http_file_t;
+
+#define hf_fd(hf) ((hf)->hf_hc->hc_fd)
 
 /**
  * De-escape HTTP URL
@@ -278,30 +382,32 @@ http_read_content(http_file_t *hf)
   int s, csize;
   char *buf;
   char chunkheader[100];
+  http_connection_t *hc = hf->hf_connection;
 
   if(hf->hf_chunked_transfer) {
     buf = NULL;
     s = 0;
 
     while(1) {
-      if(tcp_read_line(hf->hf_fd, chunkheader, sizeof(chunkheader),
-		       &hf->hf_spill) < 0)
+      if(tcp_read_line(hc->hc_fd, chunkheader, sizeof(chunkheader),
+		       &hc->hc_spill) < 0)
 	break;
  
       if((csize = strtol(chunkheader, NULL, 16)) == 0)
 	return buf;
 
       buf = realloc(buf, s + csize + 1);
-      if(tcp_read_data(hf->hf_fd, buf + s, csize, &hf->hf_spill))
+      if(tcp_read_data(hc->hc_fd, buf + s, csize, &hc->hc_spill))
 	break;
 
       s += csize;
       buf[s] = 0;
 
-      if(tcp_read_data(hf->hf_fd, chunkheader, 2, &hf->hf_spill))
+      if(tcp_read_data(hc->hc_fd, chunkheader, 2, &hc->hc_spill))
 	break;
     }
     free(buf);
+    hf->hf_chunked_transfer = 0;
     return NULL;
   }
 
@@ -309,10 +415,11 @@ http_read_content(http_file_t *hf)
   buf = malloc(s + 1);
   buf[s] = 0;
   
-  if(tcp_read_data(hf->hf_fd, buf, s, &hf->hf_spill)) {
+  if(tcp_read_data(hc->hc_fd, buf, s, &hc->hc_spill)) {
     free(buf);
     return NULL;
   }
+  hf->hf_rsize = 0;
   return buf;
 }
 
@@ -364,22 +471,23 @@ http_tokenize(char *buf, char **vec, int vecsize, int delimiter)
  *
  */
 static int
-http_read_respone(http_file_t *hf)
+http_read_response(http_file_t *hf)
 {
   int li;
   char *c, *q, *argv[2];
   int code = -1;
   int64_t i64;
+  http_connection_t *hc = hf->hf_connection;
   
-  hf->hf_connection_mode = CONNECTION_MODE_CLOSE;
+  hf->hf_connection_mode = CONNECTION_MODE_PERSISTENT;
   hf->hf_rsize = -1;
   hf->hf_chunked_transfer = 0;
 
   for(li = 0; ;li++) {
-    if(tcp_read_line(hf->hf_fd, hf->hf_line, sizeof(hf->hf_line),
-		     &hf->hf_spill) < 0)
+    if(tcp_read_line(hc->hc_fd, hf->hf_line, sizeof(hf->hf_line),
+		     &hc->hc_spill) < 0)
       return -1;
-    
+
     if(hf->hf_line[0] == 0)
       break;
 
@@ -466,13 +574,17 @@ http_read_respone(http_file_t *hf)
  *
  */
 static void
-http_disconnect(http_file_t *hf)
+http_detach(http_file_t *hf, int reusable)
 {
-  if(hf->hf_fd != -1) {
-    tcp_close(hf->hf_fd);
-    hf->hf_fd = -1;
+  if(hf->hf_connection == NULL)
+    return;
+
+  if(reusable) {
+    http_connection_park(hf->hf_connection);
+  } else {
+    http_connection_destroy(hf->hf_connection);
   }
-  htsbuf_queue_flush(&hf->hf_spill);
+  hf->hf_connection = NULL;
 }
 
 
@@ -493,13 +605,21 @@ redirect(http_file_t *hf, int *redircount, char *errbuf, size_t errlen)
     return -1;
   }
 
+  if(http_drain_content(hf)) {
+    snprintf(errbuf, errlen, "Connection lost");
+    return -1;
+  }
+
   free(hf->hf_url);
   hf->hf_url = hf->hf_location;
   http_deescape(hf->hf_url);
 
   hf->hf_location = NULL;
   
-  http_disconnect(hf);
+  // Location changed, must detach from connection
+  // We might still be able to reuse it if hostname+port is same
+  // But that's for some other code to figure out
+  http_detach(hf, 1);
   return 0;
 }
 
@@ -523,7 +643,8 @@ authenticate(http_file_t *hf, char *errbuf, size_t errlen)
     snprintf(errbuf, errlen, "Authentication without realm");
     return -1;
   }
-  snprintf(buf1, sizeof(buf1), "%s @ %s", hf->hf_auth_realm, hf->hf_hostname);
+  snprintf(buf1, sizeof(buf1), "%s @ %s", hf->hf_auth_realm, 
+	   hf->hf_connection->hc_hostname);
 
   r = keyring_lookup(buf1, &username, &password, NULL, 
 		     hf->hf_auth_failed > 0,
@@ -563,19 +684,16 @@ authenticate(http_file_t *hf, char *errbuf, size_t errlen)
  *
  */
 static int
-http_connect(http_file_t *hf, int probe, char *errbuf, int errlen,
-             int ignore_size)
+http_connect(http_file_t *hf, char *errbuf, int errlen)
 {
-  int port, code;
-  htsbuf_queue_t q;
-  int redircount = 0;
+  char hostname[128];
+  int port;
 
-  hf->hf_rsize = 0;
+  if(hf->hf_connection != NULL)
+    http_detach(hf, 0);
 
-  reconnect:
-  hf->hf_fd = -1;
   url_split(NULL, 0, hf->hf_authurl, sizeof(hf->hf_authurl), 
-	    hf->hf_hostname, sizeof(hf->hf_hostname), &port,
+	    hostname, sizeof(hostname), &port,
 	    hf->hf_path, sizeof(hf->hf_path), hf->hf_url);
 
   if(port < 0)
@@ -585,8 +703,28 @@ http_connect(http_file_t *hf, int probe, char *errbuf, int errlen,
   if(!hf->hf_path[0])
     strcpy(hf->hf_path, "/");
 
-  hf->hf_fd = tcp_connect(hf->hf_hostname, port, errbuf, errlen, 3000);
-  if(hf->hf_fd < 0) {
+  hf->hf_connection = http_connection_get(hostname, port, errbuf, errlen);
+
+  return hf->hf_connection ? 0 : -1;
+}
+
+
+/**
+ *
+ */
+static int
+http_open0(http_file_t *hf, int probe, char *errbuf, int errlen,
+	   int ignore_size)
+{
+  int code;
+  htsbuf_queue_t q;
+  int redircount = 0;
+
+  hf->hf_rsize = 0;
+
+  reconnect:
+
+  if(http_connect(hf, errbuf, errlen)) {
     hf->hf_size = -1;
     return -1;
   }
@@ -608,12 +746,16 @@ http_connect(http_file_t *hf, int probe, char *errbuf, int errlen,
 		 "\r\n",
 		 hf->hf_path,
 		 htsversion,
-		 hf->hf_hostname,
+		 hf->hf_connection->hc_hostname,
 		 hf->hf_auth ?: "", hf->hf_auth ? "\r\n" : "");
 
-  tcp_write_queue(hf->hf_fd, &q);
+  tcp_write_queue(hf->hf_connection->hc_fd, &q);
 
-  code = http_read_respone(hf);
+  code = http_read_response(hf);
+  if(code == -1 && hf->hf_connection->hc_reused) {
+    http_detach(hf, 0);
+    goto reconnect;
+  }
 
   switch(code) {
   case 200:
@@ -653,7 +795,10 @@ http_connect(http_file_t *hf, int probe, char *errbuf, int errlen,
 static void
 http_destroy(http_file_t *hf)
 {
-  http_disconnect(hf);
+  http_detach(hf, 
+	      hf->hf_rsize == 0 &&
+	      hf->hf_connection_mode == CONNECTION_MODE_PERSISTENT &&
+	      hf->hf_chunked_transfer == 0);
   free(hf->hf_url);
   free(hf->hf_auth);
   free(hf->hf_auth_realm);
@@ -721,7 +866,8 @@ http_index_parse(http_file_t *hf, fa_dir_t *fd, char *buf)
     /* skip parent dir links etc */
     if(strcmp(name, hrefd) == 0) {
       snprintf(url, HTTP_MAX_PATH_LEN, "http://%s:%d%s%s%s",
-               hf->hf_hostname, hf->hf_port, hf->hf_path,
+               hf->hf_connection->hc_hostname, 
+	       hf->hf_connection->hc_port, hf->hf_path,
                hrefd,
                isdir ? "/" : "");
       
@@ -748,28 +894,14 @@ http_index_fetch(http_file_t *hf, fa_dir_t *fd, char *errbuf, size_t errlen)
   htsbuf_queue_t q;
   char *buf;
   int redircount = 0;
-  
+  char hostname[256];
+
 reconnect:
   hf->hf_size = -1;
-  hf->hf_fd = -1;
-  
-  url_split(NULL, 0, hf->hf_authurl, sizeof(hf->hf_authurl), 
-	    hf->hf_hostname, sizeof(hf->hf_hostname), &hf->hf_port,
-	    hf->hf_path, sizeof(hf->hf_path), hf->hf_url);
-  
-  if(hf->hf_port < 0)
-    hf->hf_port = 80;
-  
-  /* empty path, default to "/" */ 
-  if(!hf->hf_path[0])
-    strcpy(hf->hf_path, "/");
-  
-  hf->hf_fd = tcp_connect(hf->hf_hostname, hf->hf_port,
-			  errbuf, errlen, 3000);
-  
-  if(hf->hf_fd < 0)
+
+  if(http_connect(hf, errbuf, errlen))
     return -1;
-  
+
   htsbuf_queue_init(&q, 0);
   
 again:
@@ -782,11 +914,15 @@ again:
 		 "\r\n",
 		 hf->hf_path,
 		 htsversion,
-		 hf->hf_hostname,
+		 hostname,
 		 hf->hf_auth ?: "", hf->hf_auth ? "\r\n" : "");
   
-  tcp_write_queue(hf->hf_fd, &q);
-  code = http_read_respone(hf);
+  tcp_write_queue(hf->hf_connection->hc_fd, &q);
+  code = http_read_response(hf);
+  if(code == -1 && hf->hf_connection->hc_reused) {
+    http_detach(hf, 0);
+    goto reconnect;
+  }
   
   switch(code) {
       
@@ -831,7 +967,6 @@ http_scandir(fa_dir_t *fd, const char *url, char *errbuf, size_t errlen)
   int retval;
   http_file_t *hf = calloc(1, sizeof(http_file_t));
   
-  htsbuf_queue_init(&hf->hf_spill, 0);
   hf->hf_url = strdup(url);
   
   retval = http_index_fetch(hf, fd, errbuf, errlen);
@@ -848,11 +983,9 @@ http_open_ex(fa_protocol_t *fap, const char *url, char *errbuf, size_t errlen,
 {
   http_file_t *hf = calloc(1, sizeof(http_file_t));
   
-  htsbuf_queue_init(&hf->hf_spill, 0);
-
   hf->hf_url = strdup(url);
 
-  if(!http_connect(hf, 1, errbuf, errlen, ignore_size)) {
+  if(!http_open0(hf, 1, errbuf, errlen, ignore_size)) {
     hf->h.fh_proto = fap;
     return &hf->h;
   }
@@ -896,8 +1029,10 @@ http_read(fa_handle_t *handle, void *buf, size_t size)
   for(i = 0; i < 5; i++) {
 
     /* If not connected, try to (re-)connect */
-    if(hf->hf_fd == -1 && http_connect(hf, 0, NULL, 0, 0))
+    if(hf->hf_connection == NULL && http_open0(hf, 0, NULL, 0, 0))
       return -1;
+
+    http_connection_t *hc = hf->hf_connection;
 
     if(hf->hf_rsize > 0) {
       /* We have pending data input on the socket */
@@ -920,10 +1055,10 @@ http_read(fa_handle_t *handle, void *buf, size_t size)
 		     "%s%s",
 		     hf->hf_path,
 		     htsversion,
-		     hf->hf_hostname,
+		     hc->hc_hostname,
 		     hf->hf_auth ?: "", hf->hf_auth ? "\r\n" : "");
 
-      if(size > 1024) {
+      if(size > 65536) {
 	htsbuf_qprintf(&q, "Range: bytes=%"PRId64"-\r\n\r\n", hf->hf_pos);
       } else {
 	htsbuf_qprintf(&q, 
@@ -931,17 +1066,17 @@ http_read(fa_handle_t *handle, void *buf, size_t size)
 		       hf->hf_pos, hf->hf_pos + size - 1);
       }
 
-      tcp_write_queue(hf->hf_fd, &q);
-      code = http_read_respone(hf);
+      tcp_write_queue(hc->hc_fd, &q);
+      code = http_read_response(hf);
 
       if(code != 206) {
-	http_disconnect(hf);
+	http_detach(hf, 0);
 	continue; // Try again
       }
 
-      if(hf->hf_chunked_transfer)
+      if(hf->hf_chunked_transfer) {
 	return -1; /* Not supported atm */
-
+      }
       if(hf->hf_rsize < size)
 	size = hf->hf_rsize;
 
@@ -949,15 +1084,15 @@ http_read(fa_handle_t *handle, void *buf, size_t size)
 	return size;
     }
 
-    if(!tcp_read_data(hf->hf_fd, buf, size, &hf->hf_spill)) {
+    if(!tcp_read_data(hc->hc_fd, buf, size, &hc->hc_spill)) {
       hf->hf_pos   += size;
       hf->hf_rsize -= size;
       return size;
     } else {
-      http_disconnect(hf);
+      http_detach(hf, 0);
     }
   }
-  http_disconnect(hf);
+  http_detach(hf, 0);
   return -1;
 }
 
@@ -992,9 +1127,8 @@ http_seek(fa_handle_t *handle, int64_t pos, int whence)
 
 
   if(hf->hf_pos != np) {
-
     if(hf->hf_rsize != 0)  // We've data pending on socket, disconnect
-      http_disconnect(hf);
+      http_detach(hf, 0);
     hf->hf_pos = np;
   }
 
@@ -1046,6 +1180,7 @@ http_stat(fa_protocol_t *fap, const char *url, struct stat *buf,
  *
  */
 fa_protocol_t fa_protocol_http = {
+  .fap_init  = http_init,
   .fap_flags = FAP_INCLUDE_PROTO_IN_URL,
   .fap_name  = "http",
   .fap_scan  = http_scandir,
@@ -1131,13 +1266,14 @@ parse_propfind(http_file_t *hf, htsmsg_t *xml, fa_dir_t *fd,
     if(fd != NULL) {
 
       if(strcmp(rpath, ehref)) {
+	http_connection_t *hc = hf->hf_connection;
 
-	if(hf->hf_port != 80) {
+	if(hc->hc_port != 80) {
 	  snprintf(path, HTTP_MAX_PATH_LEN, "webdav://%s:%d%s", 
-		   hf->hf_hostname, hf->hf_port, href);
+		   hc->hc_hostname, hc->hc_port, href);
 	} else {
 	  snprintf(path, HTTP_MAX_PATH_LEN, "webdav://%s%s", 
-		   hf->hf_hostname, href);
+		   hc->hc_hostname, href);
 	}
 
 	if((q = strrchr(path, '/')) != NULL) {
@@ -1219,23 +1355,8 @@ dav_propfind(http_file_t *hf, fa_dir_t *fd, char *errbuf, size_t errlen)
 
  reconnect:
   hf->hf_size = -1;
-  hf->hf_fd = -1;
 
-  url_split(NULL, 0, hf->hf_authurl, sizeof(hf->hf_authurl), 
-	    hf->hf_hostname, sizeof(hf->hf_hostname), &hf->hf_port,
-	    hf->hf_path, sizeof(hf->hf_path), hf->hf_url);
-
-  if(hf->hf_port < 0)
-    hf->hf_port = 80;
-
-  /* empty path, default to "/" */ 
-  if(!hf->hf_path[0])
-    strcpy(hf->hf_path, "/");
-
-  hf->hf_fd = tcp_connect(hf->hf_hostname, hf->hf_port,
-			  errbuf, errlen, 3000);
-
-  if(hf->hf_fd < 0)
+  if(http_connect(hf, errbuf, errlen))
     return -1;
 
   htsbuf_queue_init(&q, 0);
@@ -1252,11 +1373,15 @@ dav_propfind(http_file_t *hf, fa_dir_t *fd, char *errbuf, size_t errlen)
 		 hf->hf_path,
 		 fd != NULL ? 1 : 0,
 		 htsversion,
-		 hf->hf_hostname,
+		 hf->hf_connection->hc_hostname,
 		 hf->hf_auth ?: "", hf->hf_auth ? "\r\n" : "");
 
-  tcp_write_queue(hf->hf_fd, &q);
-  code = http_read_respone(hf);
+  tcp_write_queue(hf->hf_connection->hc_fd, &q);
+  code = http_read_response(hf);
+  if(code == -1 && hf->hf_connection->hc_reused) {
+    http_detach(hf, 0);
+    goto reconnect;
+  }
 
   switch(code) {
 
@@ -1293,6 +1418,7 @@ dav_propfind(http_file_t *hf, fa_dir_t *fd, char *errbuf, size_t errlen)
     goto again;
 
   default:
+    http_drain_content(hf);
     snprintf(errbuf, errlen, "Unhandled HTTP response %d", code);
     return -1;
   }
@@ -1308,7 +1434,7 @@ dav_stat(fa_protocol_t *fap, const char *url, struct stat *buf,
 	 char *errbuf, size_t errlen)
 {
   http_file_t *hf = calloc(1, sizeof(http_file_t));
-  htsbuf_queue_init(&hf->hf_spill, 0);
+
   hf->hf_url = strdup(url);
   
   if(dav_propfind(hf, NULL, errbuf, errlen)) {
@@ -1335,7 +1461,6 @@ dav_scandir(fa_dir_t *fd, const char *url, char *errbuf, size_t errlen)
   int retval;
   http_file_t *hf = calloc(1, sizeof(http_file_t));
 
-  htsbuf_queue_init(&hf->hf_spill, 0);
   hf->hf_url = strdup(url);
   
   retval = dav_propfind(hf, fd, errbuf, errlen);
@@ -1373,9 +1498,11 @@ http_request(const char *hostname, int port, const char *path,
   htsbuf_queue_t q;
   int code, r;
   char buf[256];
+  http_connection_t *hc;
 
-  hf->hf_fd = tcp_connect(hostname, port, errbuf, errlen, 3000);
-  if(hf->hf_fd < 0)
+ retry:
+  hc = hf->hf_connection = http_connection_get(hostname, port, errbuf, errlen);
+  if(hf->hf_connection == NULL)
     return -1;
 
   htsbuf_queue_init(&q, 0);
@@ -1404,17 +1531,21 @@ http_request(const char *hostname, int port, const char *path,
 		 htsversion,
 		 hostname);
 
-  tcp_write_queue(hf->hf_fd, &q);
+  tcp_write_queue(hf->hf_connection->hc_fd, &q);
 
-  htsbuf_queue_init(&hf->hf_spill, 0);
-
-  code = http_read_respone(hf);
+  code = http_read_response(hf);
+  if(code == -1 && hf->hf_connection->hc_reused) {
+    http_detach(hf, 0);
+    goto retry;
+  }
 
   if(code != 200) {
     snprintf(errbuf, errlen, "HTTP error: %d", code);
     http_destroy(hf);
     return -1;
   }
+
+  
 
   if(hf->hf_connection_mode == CONNECTION_MODE_CLOSE) {
     int capacity = 16384;
@@ -1428,8 +1559,7 @@ http_request(const char *hostname, int port, const char *path,
 	mem = realloc(mem, capacity + 1);
       }
 
-      r = tcp_read_data2(hf->hf_fd, mem + size, capacity - size, 
-			 &hf->hf_spill);
+      r = tcp_read_data2(hc->hc_fd, mem + size, capacity - size, &hc->hc_spill);
       if(r < 0)
 	break;
 
@@ -1445,7 +1575,7 @@ http_request(const char *hostname, int port, const char *path,
 
     char *mem = malloc(hf->hf_size + 1);
 
-    r = tcp_read_data(hf->hf_fd, mem, hf->hf_size, &hf->hf_spill);
+    r = tcp_read_data(hc->hc_fd, mem, hf->hf_size, &hc->hc_spill);
 
     if(r == -1) {
       snprintf(errbuf, errlen, "HTTP read error");
