e78d65a8694767e8781edf534943f9319af4bcb7
Andreas Öman <andreas@lonelycoder.com>
2009-12-27 15:26:39 +0000

    add doc comment

--- src/misc/jpeg.c
+++ src/misc/jpeg.c
@@ -16,6 +16,11 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * EXIF Documentation
+ * http://park2.wakwak.com/~tsuruzoh/Computer/Digicams/exif-e.html
+ */
+
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
