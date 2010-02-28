6c4de0ee545b332c7da93fbc3c1c10774eef0d30
Andreas Öman <andreas@lonelycoder.com>
2010-02-28 20:33:54 +0000

    Unbreak av_build_filter. This file should be removed eventually

--- src/ui/glw/glw_scaler.c
+++ src/ui/glw/glw_scaler.c
@@ -24,6 +24,8 @@
 #include <inttypes.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <libavcodec/avcodec.h>
 
 #include "glw_scaler.h"
 
@@ -39,11 +41,118 @@
 
 #define LINE_BUF_HEIGHT (NB_TAPS * 4)
 
+#define FELEM int16_t
+
+
 /**
- * external function from lavc, used to build polyphase filters
+ * 0th order modified bessel function of the first kind.
  */
-void av_build_filter(int16_t *filter, double factor, int tap_count,
-		     int phase_count, int scale, int type);
+static double bessel(double x){
+    double v=1;
+    double lastv=0;
+    double t=1;
+    int i;
+
+    x= x*x/4;
+    for(i=1; v != lastv; i++){
+        lastv=v;
+        t *= x/(i*i);
+        v += t;
+    }
+    return v;
+}
+
+#define FELEM_MAX INT32_MAX
+#define FELEM_MIN INT32_MIN
+
+/**
+ * builds a polyphase filterbank.
+ * @param factor resampling factor
+ * @param scale wanted sum of coefficients for each filter
+ * @param type 0->cubic, 1->blackman nuttall windowed sinc, 2..16->kaiser windowed sinc beta=2..16
+ */
+static void build_filter(FELEM *filter, double factor, int tap_count, int phase_count, int scale, int type){
+    int ph, i;
+    double x, y, w, tab[tap_count];
+    const int center= (tap_count-1)/2;
+
+    /* if upsampling, only need to interpolate, no filter */
+    if (factor > 1.0)
+        factor = 1.0;
+
+    for(ph=0;ph<phase_count;ph++) {
+        double norm = 0;
+        for(i=0;i<tap_count;i++) {
+            x = M_PI * ((double)(i - center) - (double)ph / phase_count) * factor;
+            if (x == 0) y = 1.0;
+            else        y = sin(x) / x;
+            switch(type){
+            case 0:{
+                const float d= -0.5; //first order derivative = -0.5
+                x = fabs(((double)(i - center) - (double)ph / phase_count) * factor);
+                if(x<1.0) y= 1 - 3*x*x + 2*x*x*x + d*(            -x*x + x*x*x);
+                else      y=                       d*(-4 + 8*x - 5*x*x + x*x*x);
+                break;}
+            case 1:
+                w = 2.0*x / (factor*tap_count) + M_PI;
+                y *= 0.3635819 - 0.4891775 * cos(w) + 0.1365995 * cos(2*w) - 0.0106411 * cos(3*w);
+                break;
+            default:
+                w = 2.0*x / (factor*tap_count*M_PI);
+                y *= bessel(type*sqrt(FFMAX(1-w*w, 0)));
+                break;
+            }
+
+            tab[i] = y;
+            norm += y;
+        }
+
+        /* normalize so that an uniform color remains the same */
+        for(i=0;i<tap_count;i++) {
+#ifdef CONFIG_RESAMPLE_AUDIOPHILE_KIDDY_MODE
+            filter[ph * tap_count + i] = tab[i] / norm;
+#else
+            filter[ph * tap_count + i] = av_clip(lrintf(tab[i] * scale / norm), FELEM_MIN, FELEM_MAX);
+#endif
+        }
+    }
+#if 0
+    {
+#define LEN 1024
+        int j,k;
+        double sine[LEN + tap_count];
+        double filtered[LEN];
+        double maxff=-2, minff=2, maxsf=-2, minsf=2;
+        for(i=0; i<LEN; i++){
+            double ss=0, sf=0, ff=0;
+            for(j=0; j<LEN+tap_count; j++)
+                sine[j]= cos(i*j*M_PI/LEN);
+            for(j=0; j<LEN; j++){
+                double sum=0;
+                ph=0;
+                for(k=0; k<tap_count; k++)
+                    sum += filter[ph * tap_count + k] * sine[k+j];
+                filtered[j]= sum / (1<<FILTER_SHIFT);
+                ss+= sine[j + center] * sine[j + center];
+                ff+= filtered[j] * filtered[j];
+                sf+= sine[j + center] * filtered[j];
+            }
+            ss= sqrt(2*ss/LEN);
+            ff= sqrt(2*ff/LEN);
+            sf= 2*sf/LEN;
+            maxff= FFMAX(maxff, ff);
+            minff= FFMIN(minff, ff);
+            maxsf= FFMAX(maxsf, sf);
+            minsf= FFMIN(minsf, sf);
+            if(i%11==0){
+                av_log(NULL, AV_LOG_ERROR, "i:%4d ss:%f ff:%13.6e-%13.6e sf:%13.6e-%13.6e\n", i, ss, maxff, minff, maxsf, minsf);
+                minff=minsf= 2;
+                maxff=maxsf= -2;
+            }
+        }
+    }
+#endif
+}
 
 static inline int get_phase(int pos)
 {
@@ -301,13 +410,13 @@ glw_bitmap_rescale(uint8_t *src, int src_width, int src_height, int src_stride,
 
   rc->line_buf = calloc(1, dst_width * bpp * (LINE_BUF_HEIGHT + NB_TAPS));
   
-  av_build_filter(&rc->h_filters[0][0], 
-		  (double)dst_width / (double)src_width,
-		  NB_TAPS, NB_PHASES, 1 << FILTER_BITS, 0);
-
-  av_build_filter(&rc->v_filters[0][0], 
-		  (double)dst_height / (double)src_height,
-		  NB_TAPS, NB_PHASES, 1 << FILTER_BITS, 0);
+  build_filter(&rc->h_filters[0][0], 
+	       (double)dst_width / (double)src_width,
+	       NB_TAPS, NB_PHASES, 1 << FILTER_BITS, 0);
+  
+  build_filter(&rc->v_filters[0][0], 
+	       (double)dst_height / (double)src_height,
+	       NB_TAPS, NB_PHASES, 1 << FILTER_BITS, 0);
 
   rc->h_incr = (src_width  * POS_FRAC) / dst_width;
   rc->v_incr = (src_height * POS_FRAC) / dst_height;
