6d76ea2f0a96c5a65d23262851eeb423efdeb56c
Mattias Wadman <mattias.wadman@gmail.com>
2010-03-10 20:23:27 +0000

    glw_x11 sync, F2 for stats

--- src/ui/glw/glw_cocoa.m
+++ src/ui/glw/glw_cocoa.m
@@ -121,6 +121,7 @@ static const struct {
    */
   
   { NSF1FunctionKey,         0,   ACTION_MENU },
+  { NSF2FunctionKey,         0,   ACTION_SHOW_MEDIA_STATS },
   
   { NSF1FunctionKey,          NSShiftKeyMask,   ACTION_PREV_TRACK },
   { NSF2FunctionKey,          NSShiftKeyMask,   ACTION_PLAYPAUSE },
