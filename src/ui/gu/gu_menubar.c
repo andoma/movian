a7500e8dd17e4614d099bab7c9550d3216a09f7c
Andreas Öman <andreas@lonelycoder.com>
2010-04-28 20:06:09 +0000

    Remove incorrect ellipsis

--- src/ui/gu/gu_menubar.c
+++ src/ui/gu/gu_menubar.c
@@ -196,7 +196,7 @@ gu_menubar_File(gu_window_t *gw, GtkAccelGroup *accel_group)
 		   gu_menu_accel_path("<Showtime-Main>/_File/OpenDir", 
 				      GDK_D, GDK_CONTROL_MASK), TRUE);
 
-  gu_menu_add_item(m, "_Close Window...", 
+  gu_menu_add_item(m, "_Close Window", 
 		   m_closewindow, gw, GTK_STOCK_OPEN,
 		   gu_menu_accel_path("<Showtime-Main>/_File/CloseWindow", 
 				      GDK_W, GDK_SHIFT_MASK | 
