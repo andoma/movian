d7b52e7f714191c8a4fe55ee67fe0c025b05f3d5
Andreas Öman <andreas@lonelycoder.com>
2010-08-15 07:54:08 +0000

    Add search to SVT play

--- plugins/svtplay/svtplay.js
+++ plugins/svtplay/svtplay.js
@@ -25,34 +25,36 @@
   var svtplay    = new Namespace("http://xml.svtplay.se/ns/playrss");
   var media      = new Namespace("http://search.yahoo.com/mrss");
 
-  function request(path, offset) {
-    var v = showtime.httpGet("http://xml.svtplay.se" + path, {
-      start: offset
-    });
-    return v;
+  // Loop over an item and return the best (highest bitrate) available media
+  function bestMedia(item) {
+    var bestItem;
+    for each (var m in item.media::group.media::content) {
+      if(showtime.canHandle(m.@url)) {
+	if(!bestItem || m.@bitrate > bestItem.@bitrate) {
+	  bestItem = m;
+	}
+      }
+    }
+    return bestItem;
   }
 
-  function svtTitle(page, url) {
+
+
+  function pageController(page, loader, populator) {
     var offset = 1;
 
-    function loader() {
+    function paginator() {
 
       var num = 0;
 
       while(1) {
-	var doc = new XML(request(url, offset + num));
+	var doc = new XML(loader(offset + num));
 	page.entries = doc.channel.opensearch::totalResults;
 	var c = 0;
 
 	for each (var item in doc.channel.item) {
 	  c++;
-
-	  var metadata = {
-	    title: item.title
-	  };
-
-	  page.appendItem("svtplay:video:" + item.svtplay::titleId,
-			  "directory", metadata);
+	  populator(page, item);
 	}
 	num += c;
 	if(c == 0 || num > 50)
@@ -63,67 +65,63 @@
     }
 
     page.type = "directory";
-    loader();
+    paginator();
     page.loading = false;
-    page.paginator = loader;
+    page.paginator = paginator;
   }
 
-
-  function svtVideo(page, url) {
-    var offset = 1;
-
-    function bestMedia(item) {
-      
-      var bestItem;
-
-      for each (var m in item.media::group.media::content) {
-	if(showtime.canHandle(m.@url)) {
-	  if(!bestItem || m.@bitrate > bestItem.@bitrate) {
-	    bestItem = m;
-	  }
-	}
-      }
-      return bestItem;
-    }
-
-
-    function loader() {
-      var doc = new XML(request(url, offset));
-      page.entries = doc.channel.opensearch::totalResults;
-
-      for each (var item in doc.channel.item) {
-	offset++;
-
-	var metadata = {
-	  title: item.svtplay::titleName.toString() + " - " + item.title
-	};
-
-	var best = bestMedia(item);
-
-	if(best === undefined)
-	  continue;
-
-	metadata.duration = parseInt(best.@duration);
-	page.appendItem(best.@url,
-			"video", metadata);
-      }
-    }
-
-    page.type = "directory";
-    loader();
-    page.loading = false;
-    page.paginator = loader;
-  }
+  function titlePopulator(page, item) {
+    page.appendItem("svtplay:video:" + item.svtplay::titleId,
+		    "directory", {
+		      title: item.title});
+  };
 
 
 
   plugin.addURI("svtplay:title:([0-9]*)", function(page, id) {
-    svtTitle(page, "/v1/title/list/" + id);
+    pageController(page, function(offset) {
+      return showtime.httpGet("http://xml.svtplay.se/v1/title/list/" + id, {
+	start: offset
+      });
+    }, titlePopulator);
   });
 
+
+
   plugin.addURI("svtplay:video:([0-9]*)", function(page, id) {
-    svtVideo(page, "/v1/video/list/" + id);
+    pageController(page, function(offset) {
+      return showtime.httpGet("http://xml.svtplay.se/v1/video/list/" + id, {
+	start: offset
+      });
+    }, function(page, item) {
+
+      var metadata = {
+	title: item.svtplay::titleName.toString() + " - " + item.title
+      };
+      
+      var best = bestMedia(item);
+      
+      if(best === undefined)
+	return;
+      
+      metadata.duration = parseInt(best.@duration);
+      page.appendItem(best.@url,
+		      "video", metadata);
+    });
   });
+  
+
+  plugin.addSearcher(
+    "SVT Play", plugin.config.path + "svtplay_icon.png",
+    function(page, query) {
+      
+      pageController(page, function(offset) {
+	return showtime.httpGet("http://xml.svtplay.se/v1/title/search/96238", {
+	  start: offset,
+	  q: query
+	});
+      }, titlePopulator);
+    });
 
 
 })(this);
