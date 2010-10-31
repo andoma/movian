b354cd30928432552c95ca402ffb2150bf9e7a8d
Andreas Öman <andreas@lonelycoder.com>
2010-10-31 21:34:41 +0000

    Add copyright statements

--- plugins/headweb/headweb.js
+++ plugins/headweb/headweb.js
@@ -1,3 +1,25 @@
+/*
+ *  Headweb plugin
+ *
+ *  API docs available here: http://opensource.headweb.com/api
+ *
+ *  Copyright (C) 2010 Andreas Öman
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 (function(plugin) {
   var PREFIX = "headweb:"
 
@@ -21,11 +43,10 @@
   });
 
   /**
-     * Headweb API KEY.
-     * Please don't steal Showtime's key..
-     * Send an email to api@headweb.com and you'll get your own for free,
-     * documentation can be found at http://opensource.headweb.com/api
-     */
+   * Headweb API KEY.
+   * Please don't steal Showtime's key..
+   * Send an email to api@headweb.com and you'll get your own for free,
+   */
   var APIKEY = "2d6461dd322b4b84b5bac8c654ee6195";
 
   /*
