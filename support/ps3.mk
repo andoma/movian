88c060cc9c3a24b048d69aeb3a1c046b516116f8
Andreas Öman <andreas@lonelycoder.com>
2011-01-23 13:21:13 +0100

    En par with current toolchain

--- support/ps3.mk
+++ support/ps3.mk
@@ -1,18 +1,18 @@
 TITLE := Showtime
-SFO := $(PSL1GHT)/bin/sfo.py
-PKG := $(PSL1GHT)/bin/pkg.py
+SFO := $(PSL1GHT)/host/bin/sfo.py
+PKG := $(PSL1GHT)/host/bin/pkg.py
 ICON0 := $(TOPDIR)/support/ps3icon.png
 APPID		:=	HTSS00003
 CONTENTID	:=	UP0001-$(APPID)_00-0000000000000000
 
-SFOXML          := $(PSL1GHT)/bin/sfo.xml
+SFOXML          := $(PSL1GHT)/host/bin/sfo.xml
 
 BIN=${BUILDDIR}/showtime.elf
 
 
 ${BIN}: ${BUILDDIR}/showtime
 	${STRIP} -o $@ $<
-	${PSL1GHT}/bin/sprxlinker $@
+	sprxlinker $@
 
 $(BUILDDIR)/showtime.pkg: ${BIN}
 	@mkdir -p $(BUILDDIR)/pkg
@@ -21,7 +21,7 @@ $(BUILDDIR)/showtime.pkg: ${BIN}
 	make_self_npdrm ${BIN} $(BUILDDIR)/pkg/USRDIR/EBOOT.BIN $(CONTENTID)
 
 	$(SFO) --title "$(TITLE)" --appid "$(APPID)" -f $(SFOXML) $(BUILDDIR)/pkg/PARAM.SFO
-	$(PKG) --contentid $(CONTENTID) $(BUILDDIR)/pkg/ $(BUILDDIR)/showtime.pkg
+	$(PKG) --contentid $(CONTENTID) $(BUILDDIR)/pkg/ $@
 	package_finalize $(BUILDDIR)/showtime.pkg
 
 pkg: $(BUILDDIR)/showtime.pkg
