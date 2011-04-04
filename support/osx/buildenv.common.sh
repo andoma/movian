2e51fba7cb9f2bcf29800521cf9b683420c50120
Mattias Wadman <mattias.wadman@gmail.com>
2011-04-04 12:49:12 +0200

    Cleanup build script

--- support/osx/buildenv.common.sh
+++ support/osx/buildenv.common.sh
@@ -1,16 +1,10 @@
-
 # pkg-config 0.18 or higher, otherwise PKG_CONFIG_LIBDIR does not work
 export PKGCONFIG=/opt/local/bin/pkg-config
-export INSTALLDIR="$HOME/showtime-env-$OSX_TARGET-$CC-$CC_ARCH"
+export INSTALLDIR="$HOME/src/showtime-env/install-$OSX_TARGET-$CC-$CC_ARCH"
 export PKG_CONFIG_LIBDIR="$INSTALLDIR/lib/pkgconfig"
 export CFLAGS="-arch $CC_ARCH -isysroot $OSX_SYSROOT"
 export LDFLAGS="-arch $CC_ARCH -mmacosx-version-min=$OSX_TARGET -isysroot $OSX_SYSROOT"
 # CC exported from sourcing script
-
-OPENSSL_TARGET="darwin-$CC_ARCH-cc"
-if [ "$CC_ARCH" = "x86_64" ] ; then
-  OPENSSL_TARGET="darwin64-x86_64-cc"
-fi
 
 (cd freetype* && \
   ./configure "--prefix=$INSTALLDIR" \
