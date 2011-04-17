1349903699854c9374aeb0d679949056fc75a928
Andreas Öman <andreas@lonelycoder.com>
2011-04-17 21:14:05 +0200

    Drop SVN support for getver.sh (and no longer print GIT-)

--- support/getver.sh
+++ support/getver.sh
@@ -3,14 +3,7 @@ revision=`cd "$1" && cat debian/changelog |head -1|grep -v current`
 test "$revision" && revision=`cd "$1" && cat debian/changelog |head -1|cut -f2 -d' '|sed s/\(//|sed s/\)//`
 
 if ! test $revision; then
-    revision=`cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
-    test $revision || revision=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
-    test $revision || revision=`cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
-    test $revision && revision=SVN-r$revision
-fi    
-if ! test $revision; then
     test $revision || revision=`cd "$1" && git describe --dirty --abbrev=5 2>/dev/null | sed  -e 's/-/./g'`
-    test $revision && revision=GIT-$revision
 fi
 
 echo $revision
