adc2c80f474b669aa06595143cbacef452e509cf
Andreas Öman <andreas@lonelycoder.com>
2011-05-09 09:24:12 +0200

    Fix invalid error message when URI does not resolv

--- src/navigator.c
+++ src/navigator.c
@@ -361,7 +361,7 @@ nav_open0(navigator_t *nav, const char *url, const char *view, prop_t *origin)
 
   nav_insert_page(nav, np, origin);
   if(backend_open(np->np_prop_root, url))
-    nav_open_errorf(np->np_prop_root, url, "No handler for URL");
+    nav_open_errorf(np->np_prop_root, "No handler for URL");
 
 }
 
