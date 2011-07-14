08c61e0767459d96ff00f1cc2040c8110c82bf58
Andreas Öman <andreas@lonelycoder.com>
2011-07-14 23:06:53 +0200

    Initial stab on plugin browse

--- src/plugins.c
+++ src/plugins.c
@@ -20,10 +20,13 @@
 #include "fileaccess/fileaccess.h"
 #include "plugins.h"
 #include "htsmsg/htsmsg_json.h"
+#include "backend/backend.h"
 
 #if ENABLE_SPIDERMONKEY
 #include "js/js.h"
 #endif
+
+static hts_mutex_t repo_mutex;
 
 
 /**
@@ -141,9 +144,162 @@ void
 plugins_init(void)
 {
   char errbuf[256];
+
+  hts_mutex_init(&repo_mutex);
+
   if(plugin_scandir(SHOWTIME_PLUGINS_URL, errbuf, sizeof(errbuf))) {
     TRACE(TRACE_ERROR, "plugins", "Unable to scan default plugindir %s -- %s",
 	  SHOWTIME_PLUGINS_URL, errbuf);
   }
-
 }
+
+
+
+
+/**
+ *
+ */
+static int
+plugin_canhandle(const char *url)
+{
+  return !strncmp(url, "plugin:", strlen("plugin:"));
+}
+
+
+#define PLUGIN_REPO_URL "http://localhost:8000/files/plugins.json"
+
+/**
+ *
+ */
+static htsmsg_t *
+repo_get(char *errbuf, size_t errlen)
+{
+  char *result;
+  static htsmsg_t *repository;
+
+  hts_mutex_lock(&repo_mutex);
+  
+  if(repository != NULL) {
+    hts_mutex_unlock(&repo_mutex);
+    return repository;
+  }
+
+  if(http_request(PLUGIN_REPO_URL, NULL, &result, NULL, errbuf, errlen,
+		  NULL, NULL, 0, NULL, NULL, NULL)) {
+    hts_mutex_unlock(&repo_mutex);
+    return NULL;
+  }
+  
+  repository = htsmsg_json_deserialize(result);
+  free(result);
+
+  hts_mutex_unlock(&repo_mutex);
+
+  if(repository == NULL)
+    snprintf(errbuf, errlen, "Invalid JSON");
+
+  return repository;
+}
+
+
+/**
+ *
+ */
+static prop_t *
+plugin_prop_from_htsmsg(htsmsg_t *pm)
+{
+  char url[256];
+  const char *id = htsmsg_get_str(pm, "id");
+  const char *title = htsmsg_get_str(pm, "title");
+
+  if(id == NULL || title == NULL)
+    return NULL;
+
+  snprintf(url, sizeof(url), "plugin:repo:%s", id);
+
+  prop_t *p = prop_create_root(NULL);
+  prop_t *metadata = prop_create(p, "metadata");
+
+  prop_set_string(prop_create(p, "type"), "plugin");
+  prop_set_string(prop_create(p, "url"), url);
+
+  prop_set_string(prop_create(metadata, "title"), title);
+  prop_set_string(prop_create(metadata, "icon"),
+		  htsmsg_get_str(pm, "icon"));
+  return p;
+}
+
+
+/**
+ *
+ */
+static void
+plugin_open_repo(prop_t *page)
+{
+  char errbuf[200];
+  htsmsg_t  *repo;
+  
+  if((repo = repo_get(errbuf, sizeof(errbuf))) == NULL) {
+    nav_open_errorf(page, "Unable to request plugin repository: %s", 
+		    errbuf);
+    return;
+  }
+
+  prop_t *model = prop_create(page, "model");
+  prop_set_string(prop_create(model, "type"), "directory");
+  prop_set_string(prop_create(model, "contents"), "items");
+
+  prop_t *metadata = prop_create(model, "metadata");
+  
+  prop_set_string(prop_create(metadata, "title"),
+		  "Available plugins");
+
+  prop_t *nodes = prop_create(model, "nodes");
+
+  htsmsg_field_t *f;
+  HTSMSG_FOREACH(f, repo) {
+    htsmsg_t *pm =  htsmsg_get_map_by_field(f);
+    if(pm == NULL)
+      continue;
+
+    prop_t *p = plugin_prop_from_htsmsg(pm);
+    if(p == NULL)
+      continue;
+
+    if(prop_set_parent(p, nodes))
+      prop_destroy(p);
+  }
+}
+
+
+/**
+ *
+ */
+static int
+plugin_open_url(prop_t *page, const char *url)
+{
+  const char *s;
+  if(!strcmp(url, "plugin:repo")) {
+    plugin_open_repo(page);
+    return 0;
+  }
+
+  if((s = mystrbegins(url, "plugin:repo:")) != NULL) {
+
+    return 0;
+  }
+
+  nav_open_errorf(page, "Invalud URI");
+  return 0;
+}
+
+
+/**
+ *
+ */
+static backend_t be_plugin = {
+  .be_canhandle = plugin_canhandle,
+  .be_open = plugin_open_url,
+};
+
+BE_REGISTER(plugin);
