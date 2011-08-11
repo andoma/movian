b8bce2517a5d1133d1eee906dedbfe767ac5f5f6
Andreas Öman <andreas@lonelycoder.com>
2011-08-11 14:54:07 +0200

    Deal with "git describe" that does not support --dirty

--- support/getver.sh
+++ support/getver.sh
@@ -6,4 +6,8 @@ if ! test $revision; then
     test $revision || revision=`cd "$1" && git describe --dirty --abbrev=5 2>/dev/null | sed  -e 's/-/./g'`
 fi
 
+if ! test $revision; then
+    test $revision || revision=`cd "$1" && git describe --abbrev=5 2>/dev/null | sed  -e 's/-/./g'`
+fi
+
 echo $revision
