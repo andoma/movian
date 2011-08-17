ddefab6ca9d1cd79975b2e1d5b62f6948057dd72
Jonas Karlsson <jonas.karlsson@fxdev.com>
2011-08-17 16:13:48 +0200

    add showtime desktop icon to the linux make install script and correct system path for fedora

--- support/linux.mk
+++ support/linux.mk
@@ -1,16 +1,53 @@
 
-prefix ?= $(INSTALLPREFIX)
-INSTBIN= $(prefix)/bin
-INSTMAN= $(prefix)/share/man1
-MAN=man/showtime.1
+# Ubuntu and Fedora uses different path
+# checking if it is a fedora release
 
-install: ${PROG} ${MAN}
+fedora := $(wildcard /etc/fedora-release)
+
+MAN=man/showtime.1
+DESKTOP=support/gnome/showtime.desktop
+ICON=support/gnome/showtime.svg
+
+ifeq ($(strip $(fedora)),)
+	# ubuntu
+	prefix ?= $(INSTALLPREFIX)/local
+else
+	# fedora
+	prefix ?= $(INSTALLPREFIX)
+endif
+
+INSTBIN= $(prefix)/bin
+INSTDESKTOP= $(prefix)/share/applications
+INSTICON= $(prefix)/share/icons/hicolor/scalable/apps
+
+
+ifeq ($(strip $(fedora)),)
+	# ubuntu
+	INSTMAN= $(prefix)/share/man1
+else
+	# fedora
+	INSTMAN= $(prefix)/share/man/man1
+endif
+
+   
+install: ${PROG} ${MAN} ${DESKTOP} ${ICON}
 	mkdir -p ${DESTDIR}$(INSTBIN)
 	install -s ${PROG} ${DESTDIR}$(INSTBIN)
 
 	mkdir -p ${DESTDIR}$(INSTMAN)
 	install ${MAN} ${DESTDIR}$(INSTMAN)
 
+	mkdir -p ${DESTDIR}$(INSTDESKTOP)
+	install ${DESKTOP} ${DESTDIR}$(INSTDESKTOP)
+
+	mkdir -p ${DESTDIR}$(INSTICON)
+	install ${ICON} ${DESTDIR}$(INSTICON)
+
+
+
 uninstall:
 	rm -f ${DESTDIR}$(INSTBIN)/${PROG}
-	rm -f ${DESTDIR}$(INSTMAN)/${MAN}
+	rm -f ${DESTDIR}$(INSTMAN)/${MAN}		
+	rm -f ${DESTDIR}${INSTDESKTOP}/${DESKTOP}
+	rm -f ${DESTDIR}${INSTICON}/${ICON}
+
