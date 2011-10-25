9ea8e383682366d7628ceaf6b0b51579e51ecb29
Andreas Öman <andreas@lonelycoder.com>
2011-10-25 23:20:43 +0200

    gu: Initial stab of a per-page dynamic menu

--- src/ui/gu/gu_toolbar.c
+++ src/ui/gu/gu_toolbar.c
@@ -17,6 +17,7 @@
  */
 
 #include <string.h>
+#include <assert.h>
 #include "navigator.h"
 #include "gu.h"
 #include "showtime.h"
@@ -143,14 +144,313 @@ toolbar_dtor(GtkObject *object, gpointer user_data)
 }
 
 
+typedef enum {
+  DMI_NONE,
+  DMI_TOGGLE,
+  DMI_SEPARATOR,
+} dmi_type_t;
+
+
+/**
+ *
+ */
+typedef struct dyn_menu {
+  GtkWidget *dm_menu;
+  gu_cloner_t dm_nodes;
+  prop_sub_t *dm_node_sub;
+} dyn_menu_t;
+
+
+
+
+typedef struct dyn_menu_item {
+  gu_cloner_node_t dmi_gcn;
+
+  GtkWidget *dmi_widget;
+  
+  dmi_type_t dmi_type;
+  char *dmi_title;
+  int dmi_enabled;
+  int dmi_value;
+
+  prop_sub_t *dmi_type_sub;
+  prop_sub_t *dmi_enabled_sub;
+
+  prop_sub_t *dmi_title_sub;
+  prop_sub_t *dmi_value_sub;
+
+  dyn_menu_t *dmi_dm;
+
+  prop_t *dmi_prop;
+
+} dyn_menu_item_t;
+
+
+
+/**
+ *
+ */
+static void
+dmi_toggle_cb(GtkCheckMenuItem *menuitem, gpointer aux)
+{
+  dyn_menu_item_t *dmi = aux;
+  int set = gtk_check_menu_item_get_active(menuitem);
+  prop_set_int_ex(prop_create(dmi->dmi_prop, "value"),
+		  dmi->dmi_value_sub, set);
+}
+
+
+/**
+ *
+ */
+static void
+dmi_create_toggle(dyn_menu_item_t *dmi)
+{
+  GtkWidget *w;
+
+  if(dmi->dmi_title == NULL)
+    return;
+
+  assert(dmi->dmi_widget == NULL);
+
+  w = gtk_check_menu_item_new_with_mnemonic(dmi->dmi_title);
+
+  gtk_widget_set_sensitive(w, dmi->dmi_enabled);
+  gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(w), dmi->dmi_value);
+  gtk_widget_show(w);
+
+  gtk_menu_shell_append(GTK_MENU_SHELL(dmi->dmi_dm->dm_menu), w);
+
+  g_signal_connect(G_OBJECT(w), "toggled", (void *)dmi_toggle_cb, dmi);
+  dmi->dmi_widget = w;
+}
+
+
+/**
+ *
+ */
+static void
+dmi_create_separator(dyn_menu_item_t *dmi)
+{
+  GtkWidget *w = gtk_separator_menu_item_new();
+  gtk_menu_shell_append(GTK_MENU_SHELL(dmi->dmi_dm->dm_menu), w);
+  gtk_widget_show(w);
+  dmi->dmi_widget = w;
+}
+
+
+/**
+ *
+ */
+static void
+dmi_create(dyn_menu_item_t *dmi)
+{
+  switch(dmi->dmi_type) {
+  case DMI_NONE:
+    return;
+  case DMI_TOGGLE:
+    return dmi_create_toggle(dmi);
+  case DMI_SEPARATOR:
+    return dmi_create_separator(dmi);
+  }
+}
+
+/**
+ *
+ */
+static void
+dmi_set_type(void *opaque, const char *str)
+{
+  dyn_menu_item_t *dmi = opaque;
+  dmi_type_t nt;
+
+  if(str == NULL)
+    nt = DMI_NONE;
+  else if(!strcmp(str, "toggle"))
+    nt = DMI_TOGGLE;
+  else if(!strcmp(str, "separator"))
+    nt = DMI_SEPARATOR;
+  else
+    nt = DMI_NONE;
+
+  if(nt == dmi->dmi_type)
+    return;
+
+  if(dmi->dmi_widget != NULL) {
+    gtk_widget_destroy(dmi->dmi_widget);
+    dmi->dmi_widget = NULL;
+  }
+
+  dmi->dmi_type = nt;
+  dmi_create(dmi);
+}
+
+
+/**
+ *
+ */
+static void
+dmi_set_title(void *opaque, const char *str)
+{
+  dyn_menu_item_t *dmi = opaque;
+
+  if(!strcmp(str ?: "", dmi->dmi_title ?: ""))
+    return;
+
+  free(dmi->dmi_title);
+  dmi->dmi_title = str ? strdup(str) : NULL;
+  dmi_create(dmi);
+}
+
+
+/**
+ *
+ */
+static void
+dmi_set_enabled(void *opaque, int v)
+{
+  dyn_menu_item_t *dmi = opaque;
+
+  dmi->dmi_enabled = v;
+  if(dmi->dmi_widget != NULL)
+    gtk_widget_set_sensitive(dmi->dmi_widget, v);
+}
+
+
+/**
+ *
+ */
+static void
+dmi_set_value(void *opaque, int v)
+{
+  dyn_menu_item_t *dmi = opaque;
+
+  dmi->dmi_value = v;
+  if(dmi->dmi_widget == NULL)
+    return;
+
+  switch(dmi->dmi_type) {
+  case DMI_NONE:
+    return;
+  case DMI_TOGGLE:
+    gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(dmi->dmi_widget),
+				   dmi->dmi_value);
+    return;
+  case DMI_SEPARATOR:
+    return;
+  }
+}
+
+
+/**
+ *
+ */
+static void
+dyn_menu_item_add(gtk_ui_t *gu, dyn_menu_t *dm,
+		  prop_t *p, dyn_menu_item_t *dmi, dyn_menu_item_t *before,
+		  int position)
+{
+  dmi->dmi_dm = dm;
+
+  dmi->dmi_prop = prop_ref_inc(p);
+
+  dmi->dmi_type_sub =
+    prop_subscribe(0,
+		   PROP_TAG_NAME("node", "type"),
+		   PROP_TAG_CALLBACK_STRING, dmi_set_type, dmi,
+		   PROP_TAG_NAMED_ROOT, p, "node",
+		   PROP_TAG_COURIER, gu->gu_pc,
+		   NULL);
+
+  dmi->dmi_title_sub =
+    prop_subscribe(0,
+		   PROP_TAG_NAME("node", "title"),
+		   PROP_TAG_CALLBACK_STRING, dmi_set_title, dmi,
+		   PROP_TAG_NAMED_ROOT, p, "node",
+		   PROP_TAG_COURIER, gu->gu_pc,
+		   NULL);
+
+  dmi->dmi_enabled_sub =
+    prop_subscribe(0,
+		   PROP_TAG_NAME("node", "enabled"),
+		   PROP_TAG_CALLBACK_INT, dmi_set_enabled, dmi,
+		   PROP_TAG_NAMED_ROOT, p, "node",
+		   PROP_TAG_COURIER, gu->gu_pc,
+		   NULL);
+
+  dmi->dmi_value_sub =
+    prop_subscribe(0,
+		   PROP_TAG_NAME("node", "value"),
+		   PROP_TAG_CALLBACK_INT, dmi_set_value, dmi,
+		   PROP_TAG_NAMED_ROOT, p, "node",
+		   PROP_TAG_COURIER, gu->gu_pc,
+		   NULL);
+}
+
+
+/**
+ *
+ */
+static void
+dyn_menu_item_del(gtk_ui_t *gu, dyn_menu_t *dn, dyn_menu_item_t *dmi)
+{
+  if(dmi->dmi_widget != NULL)
+    gtk_widget_destroy(dmi->dmi_widget);
+
+  free(dmi->dmi_title);
+  prop_unsubscribe(dmi->dmi_type_sub);
+  prop_unsubscribe(dmi->dmi_enabled_sub);
+  prop_unsubscribe(dmi->dmi_title_sub);
+  prop_unsubscribe(dmi->dmi_value_sub);
+  prop_ref_dec(dmi->dmi_prop);
+}
+
+
+/**
+ *
+ */
+static void
+dyn_menu_destroyed(GtkObject *object, gpointer user_data)
+{
+  dyn_menu_t *dm = user_data;
+  prop_unsubscribe(dm->dm_node_sub);
+  gu_cloner_destroy(&dm->dm_nodes);
+  free(dm);
+}
+
+
 /**
  *
  */
 static void
 opts_clicked(GtkToolButton *toolbutton, gpointer user_data)
 {
-  
+  int event_time = gtk_get_current_event_time();
+  gu_tab_t *gt = user_data;
+  gtk_ui_t *gu = gt->gt_gw->gw_gu;
 
+  dyn_menu_t *dm = calloc(1, sizeof(dyn_menu_t));
+
+  dm->dm_menu = gtk_menu_new();
+  gtk_widget_show_all(dm->dm_menu);
+
+  gtk_menu_attach_to_widget(GTK_MENU(dm->dm_menu), GTK_WIDGET(toolbutton), NULL);
+
+  gtk_menu_popup(GTK_MENU(dm->dm_menu), NULL, NULL, NULL, NULL, 
+		 0, event_time);
+
+  gu_cloner_init(&dm->dm_nodes, dm, dyn_menu_item_add, dyn_menu_item_del,
+		 sizeof(dyn_menu_item_t), gu, GU_CLONER_TRACK_POSITION);
+
+  dm->dm_node_sub =
+    prop_subscribe(0,
+		   PROP_TAG_NAME("nav", "currentpage", "model", "options"),
+		   PROP_TAG_CALLBACK, gu_cloner_subscription, &dm->dm_nodes,
+		   PROP_TAG_NAMED_ROOT, gt->gt_nav, "nav",
+		   PROP_TAG_COURIER, gu->gu_pc,
+		   NULL);
+
+  g_signal_connect(dm->dm_menu, "destroy", G_CALLBACK(dyn_menu_destroyed), dm);
 }
 
 
