102bdb92952666ff87863c57163acef38804c8b1
Andreas Öman <andreas@lonelycoder.com>
2012-01-11 20:36:40 +0100

    ps3: Fix mono float (ac3, etc) audio output
    Fixes #810

--- src/audio/ps3/ps3_audio.c
+++ src/audio/ps3/ps3_audio.c
@@ -89,6 +89,14 @@ copy_buf_float(float *buf, const audio_buf_t *ab, int channels)
   int i;
   const float *src = (const float *)ab->ab_data;
 
+  if(ab->ab_channels == 1 && channels == 2) {
+    for (i = 0; i < AUDIO_BLOCK_SAMPLES; i++) {
+      *buf++ = src[i+0];
+      *buf++ = src[i+0];
+    }
+    return;
+  }
+
   if(ab->ab_channels == 5 && channels == 8) {
     for (i = 0; i < AUDIO_BLOCK_SAMPLES * 5; i+=5) {
       *buf++ = src[i+0];
@@ -254,6 +262,7 @@ ps3_audio_start(audio_mode_t *am, audio_fifo_t *af)
 	memset(&conf, 0, sizeof(conf));
 
 	switch(cur_channels) {
+	case 1:
 	case 2:
 	  achannels = 2;
 	  conf.channel = 2;
@@ -374,7 +383,8 @@ audio_ps3_init(void)
 {
   audio_mode_t *am = calloc(1, sizeof(audio_mode_t));
 
-  am->am_formats =
+  am->am_formats = 
+    AM_FORMAT_PCM_MONO |
     AM_FORMAT_PCM_STEREO | AM_FORMAT_PCM_5DOT1 | 
     AM_FORMAT_PCM_6DOT1  | AM_FORMAT_PCM_7DOT1;
   am->am_sample_rates = AM_SR_48000;
