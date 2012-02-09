f7cba780c9cb260315822dda26ac2d34b3d777cb
Andreas Öman <andoma@spotify.com>
2012-02-09 22:09:28 +0700

    Fix bug in imageset decoding

--- src/backend/backend.c
+++ src/backend/backend.c
@@ -218,13 +218,15 @@ backend_imageloader(rstr_t *url0, const image_meta_t *im,
       if(best != NULL) {
 
 	if(im->im_req_width != -1) {
-	  if(w >= im->im_req_width && w < best_width)
+	  if(w >= im->im_req_width &&
+	     (w < best_width || best_width < im->im_req_width))
 	    goto gotone;
 	  if(w < im->im_req_width && w > best_width)
 	    goto gotone;
 
 	} else if(im->im_req_height != -1) {
-	  if(h >= im->im_req_height && h < best_height)
+	  if(h >= im->im_req_height &&
+	     (h < best_height || best_height < im->im_req_height))
 	    goto gotone;
 	  if(h < im->im_req_height && h > best_height)
 	    goto gotone;
