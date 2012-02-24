6191c0421f4b3276f98aee4cc4ab46ab00a8b9ff
Andreas Öman <andreas@lonelycoder.com>
2012-02-24 20:45:18 +0700

    glw/cocoa: 'Improve' a workaround fix

--- src/ui/glw/glw_cocoa.m
+++ src/ui/glw/glw_cocoa.m
@@ -215,9 +215,11 @@ static void glw_cocoa_dispatch_event(uii_t *uii, event_t *e);
    * the first call and it is the same file as last argv argument */
   if(!gcocoa.skip_first_openfile_check) {
     gcocoa.skip_first_openfile_check = 1;
-    
-    if(_argc > 1 && strcmp(cfilename, _argv[_argc - 1]) == 0)
-      return NO;
+
+    int i;
+    for(i = 1; i < _argc; i++)
+      if(_argc > 1 && strcmp(cfilename, _argv[i]) == 0)
+	return NO;
   }
   
   /* stringWithFormat uses autorelease */
