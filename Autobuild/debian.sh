7812aacfbd8e60f3109cc39f010309571b9292c2
Andreas Öman <andreas@lonelycoder.com>
2012-05-08 11:30:46 +0200

    Add support for building on ubuntu precise

--- Autobuild/debian.sh
+++ Autobuild/debian.sh
@@ -1,26 +1,57 @@
+BUILD_DEPS=`awk 'BEGIN {cnt = 1;} /^Build-Depends:/ {split($0, line, ":");split(line[2], deps, ",");for (i in deps) {d = deps[i];sub(/^ */, "", d);sub(/ *$/, "", d);split(d, tokens, " ");packages[cnt] = tokens[1];cnt++;}} END {out = ""; for(i = 1; i <= cnt; i++) {out = out packages[i] " ";} print out; }' debian/control`
 CHANGELOG=debian/changelog
 NOW=`date -R`
 VER=`git describe | sed "s/\([0-9]*\)\.\([0-9]*\)-\([0-9]*\)-.*/\1.\2.\3/"`
-echo >${CHANGELOG} "showtime (${VER}) unstable; urgency=low"
-echo >>${CHANGELOG}
-echo >>${CHANGELOG} "  * The full changelog can be found at "
-echo >>${CHANGELOG} "    http://www.lonelycoder.com/showtime/download"
-echo >>${CHANGELOG}
-echo >>${CHANGELOG} " -- Andreas Öman <andreas@lonelycoder.com>  ${NOW}"
 
-export JOBSARGS
-export JARGS
-dpkg-buildpackage -b -us -uc
+build() 
+{
+    echo >${CHANGELOG} "showtime (${VER}) unstable; urgency=low"
+    echo >>${CHANGELOG}
+    echo >>${CHANGELOG} "  * The full changelog can be found at "
+    echo >>${CHANGELOG} "    http://www.lonelycoder.com/showtime/download"
+    echo >>${CHANGELOG}
+    echo >>${CHANGELOG} " -- Andreas Öman <andreas@lonelycoder.com>  ${NOW}"
+    
+    export JOBSARGS
+    export JARGS
+    export AUTOBUILD_CONFIGURE_EXTRA
+    dpkg-buildpackage -b -us -uc
 
-for a in ../showtime*${VER}*.deb; do
-    artifact $a deb application/x-deb `basename $a`
-    rm -f $a
-done
+    for a in ../showtime*${VER}*.deb; do
+	artifact "$a" deb application/x-deb `basename $a`
+    done
 
-for a in ../showtime*${VER}*.changes; do
-    artifact $a changes text/plain `basename $a`
-    rm -f $a
-done
+    for a in ../showtime*${VER}*.changes; do
+	artifact "$a" changes text/plain `basename $a`
+    done
+}
 
-rm -f ${CHANGELOG}
-dh_clean
+clean() 
+{
+    for a in ../showtime*${VER}*.deb; do
+	rm -f "$a"
+    done
+
+    for a in ../showtime*${VER}*.changes; do
+	rm -f "$a"
+    done
+
+    rm -f ${CHANGELOG}
+    dh_clean
+}
+
+deps() 
+{
+    if [[ $EUID -ne 0 ]]; then
+	echo "Build dependencies must be installed as root"
+	exit 1
+    fi
+    apt-get -y install ${BUILD_DEPS}
+}
+
+buildenv() 
+{
+    echo $BUILD_DEPS | shasum | awk '{print $1}'
+}
+
+eval $OP
--- Autobuild/ps3.sh
+++ Autobuild/ps3.sh
@@ -1,49 +1,82 @@
-TOOLCHAIN_URL=https://github.com/andoma/ps3toolchain/tarball/5289bf8c6ce0be91606b80da4707c77c9c810de1
-TOOLCHAIN_HASH=`echo ${TOOLCHAIN_URL} | sha1sum  | awk '{print $1}'`
-TOOLCHAIN="${WORKINGDIR}/${TOOLCHAIN_HASH}"
 
-cleanup() {
-    echo "Cleaning up"
-    rm -rf ${TOOLCHAIN}
-    exit 1
+build()
+{
+    TOOLCHAIN_URL=https://github.com/andoma/ps3toolchain/tarball/5289bf8c6ce0be91606b80da4707c77c9c810de1
+    TOOLCHAIN_HASH=`echo ${TOOLCHAIN_URL} | sha1sum  | awk '{print $1}'`
+    TOOLCHAIN="${WORKINGDIR}/${TOOLCHAIN_HASH}"
+    
+    cleanup() {
+	echo "Cleaning up"
+	rm -rf ${TOOLCHAIN}
+	exit 1
+    }
+    
+    export PS3DEV=${TOOLCHAIN}/ps3dev
+    export PSL1GHT=${TOOLCHAIN}/PSL1GHT
+    export PATH=$PATH:$PS3DEV/bin:$PS3DEV/host/ppu/bin:$PS3DEV/host/spu/bin
+    export PATH=$PATH:$PSL1GHT/host/bin
+    
+    echo "Toolchain from: '${TOOLCHAIN_URL}' Local install in: ${TOOLCHAIN}"
+    
+    if [ -d $TOOLCHAIN ]; then
+	echo "Toolchain seems to exist"
+    else
+	set +e
+	trap cleanup SIGINT
+	(
+	    set -eu
+	    mkdir -p ${TOOLCHAIN}
+	    cd ${TOOLCHAIN}
+	    curl -L "${TOOLCHAIN_URL}" | tar xfz -
+	    cd *
+	    PARALLEL=${JARGS} ./toolchain.sh 1 2 3 4 5 11 12
+	)
+	
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+	    echo "Unable to stage toolchain"
+	    cleanup
+	fi
+	trap SIGINT
+	set -e
+    fi
+
+    ./configure.ps3 ${JOBSARGS} --build=${TARGET} ${RELEASE} --cleanbuild ${AUTOBUILD_CONFIGURE_EXTRA}
+    make ${JARGS} BUILD=${TARGET} pkg self
+    artifact build.${TARGET}/showtime.self self application/octect-stream showtime.self
+    artifact build.${TARGET}/showtime.pkg pkg application/octect-stream showtime.pkg
+    artifact build.${TARGET}/showtime_geohot.pkg pkg application/octect-stream showtime-gh.pkg
 }
 
-export PS3DEV=${TOOLCHAIN}/ps3dev
-export PSL1GHT=${TOOLCHAIN}/PSL1GHT
-export PATH=$PATH:$PS3DEV/bin:$PS3DEV/host/ppu/bin:$PS3DEV/host/spu/bin
-export PATH=$PATH:$PSL1GHT/host/bin
+deps()
+{
+    DISTID=`lsb_release -si`
+    case "${DISTID}" in
+	Ubuntu)
+	    if [[ $EUID -ne 0 ]]; then
+		echo "Build dependencies must be installed as root"
+		exit 1
+	    fi
+	    apt-get install git-core build-essential autoconf bison flex libelf-dev libtool pkg-config texinfo libncurses5-dev libz-dev python-dev libssl-dev libgmp3-dev ccache zip
+	    ;;
+	*)
+	    echo "Don't know how to install deps on ${DISTID}"
+	    exit 1
+	    ;;
+    esac
+}
 
-echo "Toolchain from: '${TOOLCHAIN_URL}' Local install in: ${TOOLCHAIN}"
 
-if [ -d $TOOLCHAIN ]; then
-    echo "Toolchain seems to exist"
-else
-    set +e
-    trap cleanup SIGINT
-    (
-	set -eu
-	mkdir -p ${TOOLCHAIN}
-	cd ${TOOLCHAIN}
-	curl -L "${TOOLCHAIN_URL}" | tar xfz -
-	cd *
-	PARALLEL=${JARGS} ./toolchain.sh 1 2 3 4 5 11 12
-    )
+eval $OP
 
-    STATUS=$?
-    if [ $STATUS -ne 0 ]; then
-	echo "Unable to stage toolchain"
-	cleanup
-    fi
-    trap SIGINT
-    set -e
-fi
-
-./configure.ps3 ${JOBSARGS} --build=${TARGET} ${RELEASE} --cleanbuild --ccache
-make ${JARGS} BUILD=${TARGET} pkg self
-artifact build.${TARGET}/showtime.self self application/octect-stream showtime.self
-artifact build.${TARGET}/showtime.pkg pkg application/octect-stream showtime.pkg
-artifact build.${TARGET}/showtime_geohot.pkg pkg application/octect-stream showtime-gh.pkg
-
-#on debian/ubuntu
-#apt-get install git-core build-essential autoconf bison flex libelf-dev libtool pkg-config texinfo libncurses5-dev libz-dev python-dev libssl-dev libgmp3-dev ccache zip
-
+case $OP in 
+    build)
+	build
+	;;
+    deps)
+	deps
+	;;
+    *)
+	echo "Don't know how to handle op: ${OP}"
+	;;
+esac
