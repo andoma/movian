c2f15126fb23cfda0a974fe5dd128d68da5870d4
Andreas Öman <andreas@lonelycoder.com>
2012-05-19 14:17:10 +0200

    glw: Fix bug in flat shader

--- src/ui/glw/glsl/f_flat.glsl
+++ src/ui/glw/glsl/f_flat.glsl
@@ -3,9 +3,10 @@ precision highp float;
 #endif
 
 varying vec4 f_col_mul;
+varying vec4 f_col_mul2;
 varying vec4 f_col_off;
 
 void main()
 {
-  gl_FragColor = f_col_mul + f_col_off;
+  gl_FragColor = clamp(f_col_mul, 0.0, 1.0) * f_col_mul2 + f_col_off;
 }
