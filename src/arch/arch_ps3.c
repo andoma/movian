a9a63628883946de58cda205b6be832af00126ca
Andreas Öman <andreas@lonelycoder.com>
2012-05-29 21:56:54 +0200

    ps3: Fix fallout from font loading changes

--- src/arch/arch_ps3.c
+++ src/arch/arch_ps3.c
@@ -1103,11 +1103,16 @@ arch_get_seed(void)
 void
 arch_preload_fonts(void)
 {
-  freetype_load_font("file:///dev_flash/data/font/SCE-PS3-VR-R-LATIN2.TTF", 0);
-  freetype_load_font("file:///dev_flash/data/font/SCE-PS3-NR-R-JPN.TTF", 0);
-  freetype_load_font("file:///dev_flash/data/font/SCE-PS3-YG-R-KOR.TTF", 0);
-  freetype_load_font("file:///dev_flash/data/font/SCE-PS3-DH-R-CGB.TTF", 0);
-  freetype_load_font("file:///dev_flash/data/font/SCE-PS3-CP-R-KANA.TTF", 0);
+  freetype_load_font("file:///dev_flash/data/font/SCE-PS3-VR-R-LATIN2.TTF", 0,
+		     NULL);
+  freetype_load_font("file:///dev_flash/data/font/SCE-PS3-NR-R-JPN.TTF", 0,
+		     NULL);
+  freetype_load_font("file:///dev_flash/data/font/SCE-PS3-YG-R-KOR.TTF", 0,
+		     NULL);
+  freetype_load_font("file:///dev_flash/data/font/SCE-PS3-DH-R-CGB.TTF", 0,
+		     NULL);
+  freetype_load_font("file:///dev_flash/data/font/SCE-PS3-CP-R-KANA.TTF", 0,
+		     NULL);
 }
 
 const char *
