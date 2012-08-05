0fa0c45cf1b400ee7bf79d12a2a65ab5e0f764b4
Andreas Öman <andreas@lonelycoder.com>
2012-08-05 22:36:16 +0200

    glw/playfield: Always layout all childs

--- src/ui/glw/glw_playfield.c
+++ src/ui/glw/glw_playfield.c
@@ -217,11 +217,12 @@ glw_playfield_callback(glw_t *w, void *opaque, glw_signal_t signal, void *extra)
 	c->glw_parent_amount = GLW_MAX(v, c->glw_parent_amount - s);
       }
 
+      //      if(c->glw_parent_amount > 0 && c->glw_parent_amount < 2)
+	glw_layout0(c, rc);
+
       if(c->glw_parent_amount <= 1 && c->glw_parent_detached)
 	detach(c, NULL);
 
-      if(c->glw_parent_amount > 0 && c->glw_parent_amount < 2)
-	glw_layout0(c, rc);
     }
     break;
 
