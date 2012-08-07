a2f1b8629f83029791aa82251985f6ac8acad791
Andreas Öman <andreas@lonelycoder.com>
2012-08-07 22:48:45 +0200

    videoplayer: sidx can be NULL, deal with it

--- src/fileaccess/fa_video.c
+++ src/fileaccess/fa_video.c
@@ -465,7 +465,7 @@ video_player_loop(AVFormatContext *fctx, media_codec_t **cwvec,
 	restartpos_last = sec;
 	metadb_set_video_restartpos(canonical_url, seekbase / 1000);
 
-	if(sidx->si_nitems) {
+	if(sidx != NULL && sidx->si_nitems) {
 	  int i, j = 0;
 	  for(i = 0; i < sidx->si_nitems; j = i, i++)
 	    if(sidx->si_items[i].si_start > sec)
