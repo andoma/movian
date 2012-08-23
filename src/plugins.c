1aa4bd1e8f42764f8d088cd241b74a0dc8066c22
Fábio Ferreira <facanferff@hotmail.com>
2012-08-23 15:13:19 +0100

    Update plugins documentation

--- src/plugins.c
+++ src/plugins.c
@@ -1,6 +1,7 @@
 /*
  *  Plugin mananger
  *  Copyright (C) 2010 Andreas Öman
+ *  Copyright (C) 2012 Fábio Ferreira
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
