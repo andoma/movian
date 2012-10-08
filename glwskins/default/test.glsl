41ad6ff9d4bc42a49764181261982620e57fc4f7
Andreas Öman <andreas@lonelycoder.com>
2012-10-08 20:46:05 +0200

    TMDB: Make real sure we insert something in DB if we say we do
    Fixes #1306

---
+++ glwskins/default/test.glsl
@@ -0,0 +1,15 @@
+#ifdef GL_ES
+precision highp float;
+#endif
+
+uniform sampler2D u_t0;
+
+varying vec4 f_col_mul;
+varying vec4 f_col_mul2;
+varying vec4 f_col_off;
+varying vec4 f_tex;
+
+void main()
+{
+  gl_FragColor = texture2D(u_t0, f_tex.xy).ggga;
+}
