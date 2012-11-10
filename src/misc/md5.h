55726a92536c0dbd5eb691092bc414f3c3fe8367
Andreas Öman <andreas@lonelycoder.com>
2012-11-10 11:16:55 +0100

    Avoid usable of some deprecated functions in libav

--- src/misc/md5.h
+++ src/misc/md5.h
@@ -2,10 +2,16 @@
 
 #include <libavutil/md5.h>
 
-#define md5_decl(ctx) struct AVMD5 *ctx = alloca(av_md5_size)
+#define md5_decl(ctx) struct AVMD5 *ctx = NULL;
 
-#define md5_init(ctx) av_md5_init(ctx);
+#define md5_init(ctx) do {                      \
+  ctx = av_md5_alloc();                         \
+  av_md5_init(ctx);                             \
+  } while(0)
 
 #define md5_update(ctx, data, len) av_md5_update(ctx, data, len)
 
-#define md5_final(ctx, output) av_md5_final(ctx, output)
+#define md5_final(ctx, output) do {             \
+  av_md5_final(ctx, output);                    \
+  av_freep(&ctx);                               \
+  } while(0)
--- src/misc/sha.h
+++ src/misc/sha.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include <libavutil/sha.h>
+#include <libavutil/mem.h>
 
-#define sha1_decl(ctx) struct AVSHA *ctx = alloca(av_sha_size)
+#define sha1_decl(ctx) struct AVSHA *ctx = NULL;
 
-#define sha1_init(ctx) av_sha_init(ctx, 160);
+#define sha1_init(ctx) do {                     \
+  ctx = av_sha_alloc();                         \
+  av_sha_init(ctx, 160);                        \
+  } while(0)
 
 #define sha1_update(ctx, data, len) av_sha_update(ctx, data, len)
 
-#define sha1_final(ctx, output) av_sha_final(ctx, output)
+#define sha1_final(ctx, output) do {            \
+  av_sha_final(ctx, output);                    \
+  av_freep(&ctx);                               \
+  } while(0)
+
