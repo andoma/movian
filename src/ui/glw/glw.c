aeb3a9c0948e96490077c9305df4af84139c870b
Andreas Öman <andreas@lonelycoder.com>
2012-12-02 23:34:29 +0100

    glw: Fix broken event routing

--- src/ui/glw/glw.c
+++ src/ui/glw/glw.c
@@ -1801,12 +1801,13 @@ glw_inject_event(glw_root_t *gr, event_t *e)
 {
   prop_t *p;
 
-  if(event_is_action(e, ACTION_NAV_BACK) ||
-     event_is_action(e, ACTION_NAV_FWD) ||
-     event_is_action(e, ACTION_HOME) ||
-     event_is_action(e, ACTION_PLAYQUEUE) ||
-     event_is_action(e, ACTION_RELOAD_DATA) ||
-     event_is_type(e, EVENT_OPENURL)) {
+  if(gr->gr_current_focus == NULL && 
+     (event_is_action(e, ACTION_NAV_BACK) ||
+      event_is_action(e, ACTION_NAV_FWD) ||
+      event_is_action(e, ACTION_HOME) ||
+      event_is_action(e, ACTION_PLAYQUEUE) ||
+      event_is_action(e, ACTION_RELOAD_DATA) ||
+      event_is_type(e, EVENT_OPENURL))) {
     p = prop_get_by_name(PNVEC("nav", "eventsink"), 0,
 			 PROP_TAG_ROOT, gr->gr_prop_nav,
 			 NULL);
