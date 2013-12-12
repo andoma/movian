3cff8761b12816146c64534bdfeb6e9e4975c9a5
Andreas Öman <andoma@spotify.com>
2013-12-12 18:18:09 +0100

    Rework how we allocate the meson video decoder (Get rid of a global mutex)

--- src/video/video_meson.c
+++ src/video/video_meson.c
@@ -7,7 +7,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
-
+#include <sys/mman.h>
 
 #include "showtime.h"
 #include "video_decoder.h"
@@ -27,42 +27,52 @@
  * Meson only supports one video decoder open at the same time.
  * So we just keep stuff global here
  */
-static hts_mutex_t meson_video_mutex;
+//static hts_mutex_t meson_video_mutex;
 
 /**
  * meson_video_cond is waited on for decoders that want to get access to hardware
  */
-static hts_cond_t meson_video_cond;
+//static hts_cond_t meson_video_cond;
 
 static int meson_video_fd = -1;
 static int meson_ts_epoch;
 
+static prop_t *prop_active;
+static prop_t *prop_buffer;
 
 typedef struct meson_video_decoder {
   h264_annexb_ctx_t mvd_annexb;
   media_pipe_t *mvd_mp;
 
-  int mvd_running;
+  //  int mvd_running;
+
+  int mvd_fd;
 
 } meson_video_decoder_t;
 
 
-static meson_video_decoder_t *mvd_current;
+//static meson_video_decoder_t *mvd_current;
 
-static int
+
+/**
+ *
+ */
+static void
+writeval(const char *str, int val)
+{
+  FILE *fp = fopen(str, "w");
+  if(fp == NULL)
+    return;
+
+  fprintf(fp, "%d\n", val);
+  fclose(fp);
+}
+
+
+static void
 set_tsync_enable(int enable)
 {
-  int fd;
-  const char *path = "/sys/class/tsync/enable";
-  char  bcmd[16];
-  fd = open(path, O_CREAT | O_RDWR | O_TRUNC, 0644);
-  if (fd >= 0) {
-    sprintf(bcmd, "%d", enable);
-    write(fd, bcmd, strlen(bcmd));
-    close(fd);
-    return 0;
-  }
-  return -1;
+  writeval("/sys/class/tsync/enable", 1);
 }
 
 const static AVRational mpeg_tc = {1, 90000};
@@ -80,96 +90,113 @@ meson_set_pcr(media_pipe_t *mp, int64_t pts, int epoch, int discont)
 
   pts = av_rescale_q(pts, AV_TIME_BASE_Q, mpeg_tc);
   int r = ioctl(meson_video_fd, AMSTREAM_IOC_SET_PCRSCR, pts);
-  TRACE(TRACE_INFO, "MESON", "Audio clock set to %lld  r=%d", pts, r);
+  TRACE(TRACE_INFO, "MESON", "%s: Audio clock set to %lld  r=%d",
+        mp->mp_name, pts, r);
 }
 
 
 static void
 snapshot_test(media_pipe_t *mp)
 {
+  int64_t ts = showtime_get_ts();
   int fd = open("/dev/amvideocap0",O_RDWR);
   int ret;
-
-  int w = 512;
-  int h = 512;
+  int w = 256;
+  int h = 256;
 
   if(fd < 0) {
     perror("Unable to open amvideocap");
     return;
   }
 
-  ret = ioctl(fd, AMVIDEOCAP_IOW_SET_WANTFRAME_FORMAT, GE2D_FORMAT_S24_BGR);
-
-  if(w>0){
-    ret=ioctl(fd,AMVIDEOCAP_IOW_SET_WANTFRAME_WIDTH,w);
-  }
-  if(h>0){
-    ret=ioctl(fd,AMVIDEOCAP_IOW_SET_WANTFRAME_HEIGHT,h);
-  }
-
-  ret=ioctl(fd, AMVIDEOCAP_IOW_SET_WANTFRAME_AT_FLAGS, CAP_FLAG_AT_CURRENT);
-
   pixmap_t *pm = calloc(1, sizeof(pixmap_t));
   pm->pm_refcount = 1;
   pm->pm_width = w;
   pm->pm_height = h;
+
+
 #if 0
   pm->pm_linesize = w * 4;
   pm->pm_type = PIXMAP_BGR32;
+  ioctl(fd, AMVIDEOCAP_IOW_SET_WANTFRAME_FORMAT, GE2D_FMT_S32_RGBA);
 #else
   pm->pm_linesize = w * 3;
   pm->pm_type = PIXMAP_RGB24;
+  ioctl(fd, AMVIDEOCAP_IOW_SET_WANTFRAME_FORMAT, GE2D_FORMAT_S24_BGR);
 #endif
 
+  ioctl(fd,AMVIDEOCAP_IOW_SET_WANTFRAME_WIDTH, w);
+  ioctl(fd,AMVIDEOCAP_IOW_SET_WANTFRAME_HEIGHT, h);
+  ioctl(fd, AMVIDEOCAP_IOW_SET_WANTFRAME_AT_FLAGS, CAP_FLAG_AT_CURRENT);
 
 
   int size = pm->pm_linesize * h;
 
-  pm->pm_data = malloc(size);
-  ret = read(fd, pm->pm_data, size);
+  ret = ioctl(fd, AMVIDEOCAP_IOW_SET_START_CAPTURE, 10000);
+  if(ret) {
+    close(fd);
+    pixmap_release(pm);
+    return;
+  }
+  void *p = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
 
-  printf("Read %d bytes (of %d)\n", ret, size);
-#if 0
-  ret = ioctl(fd,AMVIDEOCAP_IOR_GET_FRAME_WIDTH,&w);
-  ret = ioctl(fd,AMVIDEOCAP_IOR_GET_FRAME_HEIGHT,&h);
-#endif
+  pm->pm_flags |= PIXMAP_MMAPED;
+  pm->pm_data = p;
   close(fd);
 
-  // Snapshot here
+  TRACE(TRACE_DEBUG, "SNAPSHOT", "Finished in %dus", (int)(showtime_get_ts() - ts));
+
   frame_info_t fi;
   memset(&fi, 0, sizeof(fi));
   fi.fi_type = 'PM';
+  fi.fi_pts = PTS_UNSET;
   fi.fi_data[0] = (void *)pm;
 
   mp->mp_video_frame_deliver(&fi, mp->mp_video_frame_opaque);
-
   pixmap_release(pm);
 }
 
+#if 0
+static void
+zero_img(media_pipe_t *mp)
+{
+  // Snapshot here
+  frame_info_t fi;
+  memset(&fi, 0, sizeof(fi));
+  fi.fi_type = 'ZERO';
+
+  mp->mp_video_frame_deliver(&fi, mp->mp_video_frame_opaque);
+}
+#endif
 
 /**
  *
  */
 static void
-amstream_stop(int snapshot)
+amstream_stop(meson_video_decoder_t *mvd, int snapshot)
 {
-  media_pipe_t *mp = mvd_current->mvd_mp;
+  media_pipe_t *mp = mvd->mvd_mp;
 
-  if(snapshot && 0)
+  TRACE(TRACE_DEBUG, "MESON", "%s: About to surrender video decoder",
+        mp->mp_name);
+
+  if(snapshot)
     snapshot_test(mp);
 
-  assert(mvd_current->mvd_running);
-  mvd_current->mvd_running = 0;
+  assert(mvd->mvd_fd != -1);
 
   hts_mutex_lock(&mp->mp_clock_mutex);
   mp->mp_set_audio_clock = NULL;
   hts_mutex_unlock(&mp->mp_clock_mutex);
 
+  prop_set_string(prop_active, "Closed");
+
+  if(snapshot)
+    usleep(40000);
   close(meson_video_fd);
+  mvd->mvd_fd = -1;
   meson_video_fd = -1;
   meson_ts_epoch = 0;
-  mvd_current = NULL;
-  hts_cond_signal(&meson_video_cond);
   TRACE(TRACE_DEBUG, "MESON", "%s: Released video decoder", mp->mp_name);
 }
 
@@ -181,21 +208,10 @@ static int
 amstream_start(media_pipe_t *mp, video_decoder_t *vd,
                meson_video_decoder_t *mvd)
 {
-  while(1) {
-    if(!vd->vd_run || vd->vd_activation >= VIDEO_ACTIVATION_PRELOAD)
-      return -1; // Not supposed to run anymore, get out of here
-
-    if(mvd_current == NULL)
-      break; // Free to acquire
-
-    hts_cond_wait_timeout(&meson_video_cond, &meson_video_mutex, 10);
-  }
-
   meson_ts_epoch = 0;
 
   int fd = open("/dev/amstream_vbuf", O_RDWR);
   if(fd == -1) {
-    perror("open(/dev/amstream_vbuf");
     return 1;
   }
 
@@ -204,20 +220,27 @@ amstream_start(media_pipe_t *mp, video_decoder_t *vd,
     close(fd);
     return 1;
   }
+  mvd->mvd_fd = fd;
   meson_video_fd = fd;
+  static int opencnt;
+  opencnt++;
+  char buf[40];
+  snprintf(buf, sizeof(buf), "Open-%d", opencnt);
+
+  prop_set_string(prop_active, buf);
 
   int64_t pts;
 
-  pts = av_rescale_q(mp->mp_audio_clock_avtime, AV_TIME_BASE_Q, mpeg_tc);
-  TRACE(TRACE_INFO, "MESON", "Audio initially set to %lld", pts);
-  ioctl(meson_video_fd, AMSTREAM_IOC_SET_PCRSCR, pts);
+  pts = av_rescale_q(mp->mp_audio_clock, AV_TIME_BASE_Q, mpeg_tc);
+  TRACE(TRACE_INFO, "MESON", "%s: Audio initially set to %lld",
+        mp->mp_name, pts);
+
+  ioctl(mvd->mvd_fd, AMSTREAM_IOC_SET_PCRSCR, pts);
 
   hts_mutex_lock(&mp->mp_clock_mutex);
   mp->mp_set_audio_clock = meson_set_pcr;
   hts_mutex_unlock(&mp->mp_clock_mutex);
 
-  mvd_current = mvd;
-  mvd->mvd_running = 1;
   return 0;
 }
 
@@ -229,20 +252,11 @@ static void
 meson_video_flush(struct media_codec *mc, struct video_decoder *vd)
 {
   meson_video_decoder_t *mvd = mc->opaque;
-#if 0
   media_pipe_t *mp = mc->mp;
-
-  if(lasting) {
-    printf("%s: Long flush\n", mp->mp_name);
-    hts_mutex_lock(&meson_video_mutex);
-    if(mvd == mvd_current)
-      stop_current(1);
-    hts_mutex_unlock(&meson_video_mutex);
-  }
-#endif
+  printf("%s: Flush\n", mp->mp_name);
 
 
-  if(mvd->mvd_running) {
+  if(mvd->mvd_fd != -1) {
     // Fix flush code
     TRACE(TRACE_ERROR, "MESON", "XXX: Need to implement flush");
   }
@@ -254,7 +268,7 @@ meson_video_flush(struct media_codec *mc, struct video_decoder *vd)
 /**
  *
  */
-static void
+static int
 submit_au(meson_video_decoder_t *mvd, void *data, size_t len,
 	  int drop_non_ref, video_decoder_t *vd, int64_t pts,
           media_pipe_t *mp)
@@ -269,12 +283,18 @@ submit_au(meson_video_decoder_t *mvd, void *data, size_t len,
 
     if(vd->vd_activation >= VIDEO_ACTIVATION_PRELOAD) {
       // No longer in decoding mode, get out of here ASAP
-      return;
+      hts_mutex_unlock(&mp->mp_mutex);
+      amstream_stop(mvd, 1);
+      hts_mutex_lock(&mp->mp_mutex);
+      return 1;
     }
 
     if(vd->vd_run == 0) {
       // We are not supposed to run anymore so bail out directly
-      return;
+      hts_mutex_unlock(&mp->mp_mutex);
+      amstream_stop(mvd, 0);
+      hts_mutex_lock(&mp->mp_mutex);
+      return 1;
     }
 
     if(ioctl(meson_video_fd, AMSTREAM_IOC_VB_STATUS, &aip)) {
@@ -286,6 +306,8 @@ submit_au(meson_video_decoder_t *mvd, void *data, size_t len,
      * We don't wanna keep to much data in the kernel side video buffer
      * so limit to 1MB
      */
+
+    if(0) prop_set_int(prop_buffer, aip.status.data_len);
 
     if(aip.status.data_len < 1000000)
       break;
@@ -318,6 +340,7 @@ submit_au(meson_video_decoder_t *mvd, void *data, size_t len,
     len -= r;
     data += r;
   }
+  return 0;
 }
 
 
@@ -331,21 +354,24 @@ meson_video_decode_locked(struct media_codec *mc, struct video_decoder *vd,
   meson_video_decoder_t *mvd = mc->opaque;
   media_pipe_t *mp = mc->mp;
 
-  hts_mutex_lock(&meson_video_mutex);
+  int cnt = 0;
 
-  if(mvd != mvd_current) {
+  while(mvd->mvd_fd == -1) {
 
-    TRACE(TRACE_DEBUG, "MESON", "%s: Want video decoder", mp->mp_name);
-    hts_mutex_unlock(&mp->mp_mutex);
+    if(!vd->vd_run || vd->vd_activation >= VIDEO_ACTIVATION_PRELOAD)
+      return; // Not supposed to run anymore, get out of here
+
     int r = amstream_start(mp, vd, mvd);
-    hts_mutex_lock(&mp->mp_mutex);
-
-    if(r == -1) {
-      hts_mutex_unlock(&meson_video_mutex);
-      return;
+    if(r) {
+      hts_mutex_unlock(&mp->mp_mutex);
+      usleep(10000);
+      cnt++;
+      if(cnt == 20)
+        printf("%s: Failed to acquire decoder\n", mp->mp_name);
+      hts_mutex_lock(&mp->mp_mutex);
     }
-    TRACE(TRACE_DEBUG, "MESON", "%s: Got video decoder", mp->mp_name);
   }
+
 
   if(mvd->mvd_annexb.extradata != NULL &&
      mvd->mvd_annexb.extradata_injected == 0) {
@@ -358,9 +384,10 @@ meson_video_decode_locked(struct media_codec *mc, struct video_decoder *vd,
   size_t size = mb->mb_size;
 
   h264_to_annexb(&mvd->mvd_annexb, &data, &size);
-  submit_au(mvd, data, size, mb->mb_skip == 1, vd, mb->mb_pts, mp);
+
+  if(submit_au(mvd, data, size, mb->mb_skip == 1, vd, mb->mb_pts, mp))
+    return;
   mp->mp_set_video_codec('mesn', mc, mp->mp_video_frame_opaque);
-  hts_mutex_unlock(&meson_video_mutex);
 }
 
 
@@ -373,12 +400,8 @@ meson_video_close(struct media_codec *mc)
 {
   meson_video_decoder_t *mvd = mc->opaque;
 
-  if(mvd->mvd_running) {
-    assert(mvd == mvd_current);
-    hts_mutex_lock(&meson_video_mutex);
-    amstream_stop(0);
-    hts_mutex_unlock(&meson_video_mutex);
-  }
+  if(mvd->mvd_fd != -1)
+    amstream_stop(mvd, 0);
 
   h264_to_annexb_cleanup(&mvd->mvd_annexb);
   free(mvd);
@@ -392,13 +415,10 @@ meson_video_relinqush(struct media_codec *mc, struct video_decoder *vd)
 {
   meson_video_decoder_t *mvd = mc->opaque;
 
-  if(!mvd->mvd_running)
+  if(mvd->mvd_fd == -1)
     return;
 
-  assert(mvd == mvd_current);
-  hts_mutex_lock(&meson_video_mutex);
-  amstream_stop(1);
-  hts_mutex_unlock(&meson_video_mutex);
+  amstream_stop(mvd, 1);
 }
 
 /**
@@ -413,6 +433,7 @@ meson_video_open(media_codec_t *mc, const media_codec_params_t *mcp,
 
   meson_video_decoder_t *mvd = calloc(1, sizeof(meson_video_decoder_t));
   mvd->mvd_mp = mp;
+  mvd->mvd_fd = -1;
 
   if(mc->codec_id == CODEC_ID_H264 && mcp != NULL && mcp->extradata_size)
     h264_to_annexb_init(&mvd->mvd_annexb, mcp->extradata, mcp->extradata_size);
@@ -432,8 +453,13 @@ meson_video_open(media_codec_t *mc, const media_codec_params_t *mcp,
 static void
 meson_video_init(void)
 {
-  hts_mutex_init(&meson_video_mutex);
-  hts_cond_init(&meson_video_cond, &meson_video_mutex);
+
+  prop_t *p = prop_get_global();
+  p = prop_create(p, "meson");
+  p = prop_create(p, "video");
+  prop_active = prop_create(p, "active");
+  prop_buffer = prop_create(p, "buffer");
+
   set_tsync_enable(1);
 }
 
