0ef0afb80e700d4a205a23cd3cad364eedf32068
Andreas Öman <andreas@lonelycoder.com>
2014-02-06 13:57:05 +0700

    smbclient: Fix broken reassembly of TRANS2

--- src/fileaccess/fa_nativesmb.c
+++ src/fileaccess/fa_nativesmb.c
@@ -1325,13 +1325,19 @@ smb_dispatch(void *aux)
         const TRANS2_reply_t *tr = (const TRANS2_reply_t *)buf;
 
         int total_count = letoh_16(tr->total_data_count);
-        int count = letoh_16(tr->param_count) + letoh_16(tr->data_count);
+        int seg_count = letoh_16(tr->param_count) + letoh_16(tr->data_count);
+        SMBTRACE("trans2 segment: "
+                 "total=%d param_count=%d data_count=%d poff=%d", 
+                 total_count,
+                 letoh_16(tr->param_count),
+                 letoh_16(tr->data_count),
+                 letoh_16(tr->param_offset));
 
-        if(count > len - sizeof(TRANS2_reply_t)) {
+        if(seg_count > len - sizeof(TRANS2_reply_t)) {
           TRACE(TRACE_ERROR, "SMB",
                 "%s:%d malformed trans2, %d > %d",
                 cc->cc_hostname, cc->cc_port,
-                count, len - sizeof(TRANS2_reply_t));
+                seg_count, len - sizeof(TRANS2_reply_t));
           goto bad_trans2;
         }
 
@@ -1361,22 +1367,22 @@ smb_dispatch(void *aux)
 
 
           nr->nr_response = buf;
-          nr->nr_response_len = count + sizeof(TRANS2_reply_t);
+          nr->nr_response_len = len;
         } else {
-          void *payload = buf + sizeof(TRANS2_reply_t);
-          if(count < 0) {
+          void *payload = buf + letoh_16(tr->param_offset);
+          if(seg_count < 0) {
             TRACE(TRACE_ERROR, "SMB",
-                  "%s:%d Unable to reassemble trans2, total_count=%d",
-                  cc->cc_hostname, cc->cc_port, count);
+                  "%s:%d Unable to reassemble trans2, seg_count=%d",
+                  cc->cc_hostname, cc->cc_port, seg_count);
             goto bad_trans2;
           }
 
           nr->nr_response = realloc(nr->nr_response,
-                                    nr->nr_response_len + count);
+                                    nr->nr_response_len + seg_count);
 
           memcpy(nr->nr_response + nr->nr_response_len,
-                 payload, count);
-          nr->nr_response_len += count;
+                 payload, seg_count);
+          nr->nr_response_len += seg_count;
           free(buf);
         }
 
