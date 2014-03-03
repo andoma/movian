b5f5481c00b36a5228e0ae71af7bca5c4fec5353
Andreas Öman <andreas@lonelycoder.com>
2014-03-03 13:24:03 +0100

    Set good defaults for cache and persistent path when running in STOS

--- src/arch/posix/posix.c
+++ src/arch/posix/posix.c
@@ -74,10 +74,14 @@ posix_init(void)
   gettimeofday(&tv, NULL);
   srand(tv.tv_usec);
 
+#ifdef STOS
+  gconf.cache_path = strdup("/stos/cache/showtime");
+  gconf.persistent_path = strdup("/stos/persistent/showtime");
+#else
   const char *homedir = getenv("HOME");
-  char buf[PATH_MAX];
 
   if(homedir != NULL) {
+    char buf[PATH_MAX];
 
     snprintf(buf, sizeof(buf), "%s/.cache/showtime", homedir);
     gconf.cache_path = strdup(buf);
@@ -85,6 +89,7 @@ posix_init(void)
     snprintf(buf, sizeof(buf), "%s/.hts/showtime", homedir);
     gconf.persistent_path = strdup(buf);
   }
+#endif
 
   setlocale(LC_ALL, "");
   decorate_trace = isatty(2);
