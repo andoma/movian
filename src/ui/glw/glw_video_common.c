61b8254846724de13027da92b7cae30b4f6e3f2e
Andreas Öman <andreas@lonelycoder.com>
2014-03-13 12:26:41 +0100

    glw/video: Improve AVDIFF trace message

--- src/ui/glw/glw_video_common.c
+++ src/ui/glw/glw_video_common.c
@@ -279,8 +279,9 @@ glw_video_compute_avdiff(glw_root_t *gr, media_pipe_t *mp,
   if(gconf.enable_detailed_avdiff) {
     static int64_t lastpts, lastaclock, lastclock;
 
-    TRACE(TRACE_DEBUG, "AVDIFF", "E:%3d %10f %10d %15"PRId64":a:%-8"PRId64" %15"PRId64":v:%-8"PRId64" %15"PRId64" %15"PRId64" %s %lld", 
+    TRACE(TRACE_DEBUG, "AVDIFF", "VE:%d AE:%d %10f %10d %15"PRId64":a:%-8"PRId64" %15"PRId64":v:%-8"PRId64" %15"PRId64" %15"PRId64" %s %lld", 
 	  epoch,
+	  mp->mp_audio_clock_epoch,
 	  gv->gv_avdiff_x,
 	  gv->gv_avdiff,
 	  aclock,
