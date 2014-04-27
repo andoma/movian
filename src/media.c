94136034846e688bdcdd5b82da05d08962d2a2e9
Andreas Öman <andreas@lonelycoder.com>
2014-04-27 07:07:41 +0200

    Fix bug when seeking in queues

--- src/media.c
+++ src/media.c
@@ -1018,6 +1018,7 @@ mp_direct_seek(media_pipe_t *mp, int64_t ts)
 
   if(!mp_seek_in_queues(mp, ts + mp->mp_start_time)) {
     prop_set(mp->mp_prop_root, "seektime", PROP_SET_FLOAT, ts / 1000000.0);
+    return;
   } else {
 
     /* If there already is a seek event enqueued, update it */
