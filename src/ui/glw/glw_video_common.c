69886a0d9fe6e5ad1b244b25c58c9904561e6271
Andreas Öman <andreas@lonelycoder.com>
2014-05-20 19:47:04 +0200

    glw: Route skip prev/next if video widget is focused
    Fixes #2172

--- src/ui/glw/glw_video_common.c
+++ src/ui/glw/glw_video_common.c
@@ -122,6 +122,8 @@ glw_video_widget_event(glw_t *w, event_t *e)
   if(event_is_action(e, ACTION_PLAYPAUSE) ||
      event_is_action(e, ACTION_PLAY) ||
      event_is_action(e, ACTION_PAUSE) ||
+     event_is_action(e, ACTION_SKIP_FORWARD) ||
+     event_is_action(e, ACTION_SKIP_BACKWARD) ||
      event_is_action(e, ACTION_SHOW_MEDIA_STATS)) {
     mp_enqueue_event(mp, e);
     return 1;
