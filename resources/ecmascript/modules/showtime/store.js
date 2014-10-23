3a70d1887b7f50d5e9dc37f7fd55015c13d1f5ed
Andreas Öman <andreas@lonelycoder.com>
2014-10-23 16:10:40 +0200

    ecmascript: Add store module for automatic persisting of an object to disk

---
+++ resources/ecmascript/modules/showtime/store.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+
+
+var storeproxy = {
+
+  get: function(obj, name) {
+    return obj.keys[name];
+  },
+
+  set: function(obj, name, value) {
+    if(obj.keys[name] === value)
+      return;
+
+    obj.keys[name] = value;
+
+    if(obj.timer)
+      clearTimeout(obj.timer);
+
+    obj.timer = setTimeout(function() {
+      fs.writeFileSync(obj.filename, JSON.stringify(obj.keys));
+      delete obj.timer;
+    }, 1000);
+
+  }
+}
+
+exports.create = function(name) {
+  var obj = {
+    filename: 'store/' + name,
+    keys: {}
+  };
+
+  Showtime.fs.mkdirs('store');
+
+  try {
+    obj.keys = JSON.parse(fs.readFileSync(obj.filename));
+  } catch (e) {
+  }
+
+  return new Proxy(obj, storeproxy);
+}
