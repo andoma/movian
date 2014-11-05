b6b818d8824e1a9e763cd59f60558cd7857f9441
Andreas Öman <andreas@lonelycoder.com>
2014-11-05 08:08:39 +0100

    navigator: Backout a change that was added during testing

---
+++ src/ecmascript/es_console.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+
+#include "showtime.h"
+#include "ecmascript.h"
+
+
+/**
+ *
+ */
+static int
+es_console_log(duk_context *ctx)
+{
+  es_context_t *ec = es_get(ctx);
+
+  const char *msg = duk_to_string(ctx, 0);
+  TRACE(TRACE_DEBUG, "ecmascript", "%s: %s", ec->ec_id, msg);
+  return 0;
+}
+
+
+
+/**
+ * Showtime object exposed functions
+ */
+const duk_function_list_entry fnlist_Showtime_console[] = {
+  { "log",                      es_console_log,           1 },
+  { NULL, NULL, 0}
+};
