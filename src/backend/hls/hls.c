4f43ae8cc5873b012f3d235c63300efaca208ff0
Andreas Öman <andreas@lonelycoder.com>
2014-12-08 23:00:18 +0100

    hls: Make resume work again
    Fixes #2400

--- src/backend/hls/hls.c
+++ src/backend/hls/hls.c
@@ -82,9 +82,12 @@ typedef struct hls_segment {
   int hs_byte_offset;
   int hs_byte_size;
   int64_t hs_duration; // in usec
+
   int64_t hs_time_offset;
-  
-  //  int64_t hs_start_ts;
+  int64_t hs_first_ts;
+  int64_t hs_ts_offset;
+
+
   int hs_seq;
 
   uint8_t hs_crypto;
@@ -184,8 +187,6 @@ typedef struct hls_demuxer {
   hls_variant_t *hd_req;
 
   int hd_bw;
-
-  int64_t hd_delta_ts;
 
   media_codec_t *hd_audio_codec;
 
@@ -338,6 +339,7 @@ static hls_segment_t *
 hv_add_segment(hls_variant_t *hv, const char *url)
 {
   hls_segment_t *hs = calloc(1, sizeof(hls_segment_t));
+  hs->hs_first_ts = PTS_UNSET;
   hs->hs_url = url_resolve_relative_from_base(hv->hv_url, url);
   hs->hs_variant = hv;
   TAILQ_INSERT_TAIL(&hv->hv_segments, hs, hs_link);
@@ -1221,12 +1223,13 @@ hls_event_callback(media_pipe_t *mp, void *aux, event_t *e)
 
     if(ets->epoch == mp->mp_epoch) {
       int sec = ets->ts / 1000000;
-      //	last_timestamp_presented = ets->ts;
+      h->h_last_timestamp_presented = ets->ts;
 
       // Update restartpos every 5 seconds
       if(mp->mp_flags & MP_CAN_SEEK &&
          (sec < h->h_restartpos_last || sec >= h->h_restartpos_last + 5)) {
         h->h_restartpos_last = sec;
+
         //	  playinfo_set_restartpos(canonical_url, ets->ts / 1000, 1);
       }
     }
@@ -1604,8 +1607,15 @@ hls_play(hls_t *h, media_pipe_t *mp, char *errbuf, size_t errlen,
         continue;
       }
 
+
       int si = pkt.stream_index;
       const AVStream *s = hv->hv_fctx->streams[si];
+      hls_segment_t *hs = hv->hv_current_seg;
+
+      if(hs != NULL && hs->hs_first_ts == PTS_UNSET && pkt.dts != PTS_UNSET) {
+        hs->hs_first_ts = rescale(hv->hv_fctx, pkt.dts, si);
+        hs->hs_ts_offset = hs->hs_first_ts - hs->hs_time_offset;
+      }
 
       if(si == hv->hv_vstream) {
 
@@ -1618,7 +1628,6 @@ hls_play(hls_t *h, media_pipe_t *mp, char *errbuf, size_t errlen,
           }
 
           if(pkt.dts == AV_NOPTS_VALUE) {
-            printf("Found keyframe but there was no DTS!!!!!\n");
             av_free_packet(&pkt);
             continue;
           }
@@ -1693,13 +1702,8 @@ hls_play(hls_t *h, media_pipe_t *mp, char *errbuf, size_t errlen,
 
       if(mb->mb_data_type == MB_VIDEO) {
 
-	if(hd->hd_delta_ts == PTS_UNSET && mb->mb_pts != PTS_UNSET) {
-	  hd->hd_delta_ts = mb->mb_pts;
-          mp->mp_start_time = hd->hd_delta_ts;
-        }
-
 	mb->mb_drive_clock = 1;
-	mb->mb_delta = hd->hd_delta_ts;
+	mb->mb_delta = hs->hs_ts_offset;
       }
 
       mb->mb_sequence = h->h_current_seq;
@@ -1907,7 +1911,6 @@ static void
 hls_demuxer_init(hls_demuxer_t *hd)
 {
   TAILQ_INIT(&hd->hd_variants);
-  hd->hd_delta_ts = PTS_UNSET;
 }
 
 
