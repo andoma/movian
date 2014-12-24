4f9775bba5a92810db90cebf306dd310c3a1fe22
Andreas Öman <andreas@lonelycoder.com>
2014-12-24 10:09:25 +0100

    glw/opengl: Remove unused code

--- src/ui/glw/glw_opengl_ogl.c
+++ src/ui/glw/glw_opengl_ogl.c
@@ -138,11 +138,5 @@ glw_opengl_init_context(glw_root_t *gr)
   const char *renderer = (const char *)glGetString(GL_RENDERER);
   TRACE(TRACE_INFO, "GLW", "OpenGL Renderer: '%s' by '%s'", renderer, vendor);
 
-  int use_shaders = 1;
-
-  if(use_shaders) {
-    return glw_opengl_shaders_init(gr, 0);
-  } else {
-    return glw_opengl_ff_init(gr);
-  }
+  return glw_opengl_shaders_init(gr, 0);
 }
