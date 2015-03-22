fa82e8844ba7090eb0ff047ac63536ee59d8b7ef
Andreas Öman <andreas@lonelycoder.com>
2015-03-22 12:01:47 +0100

    fix bad args to service_create_managed()

--- src/arch/ps3/ps3_main.c
+++ src/arch/ps3/ps3_main.c
@@ -240,8 +240,7 @@ scan_root_fs(callout_t *co, void *aux)
       }
 
       rfn->service = service_create_managed(name, name, fname, type, NULL, 0, 1,
-					    SVC_ORIGIN_MEDIA, 1,
-					    name);
+					    SVC_ORIGIN_MEDIA, name);
       LIST_INSERT_HEAD(&rootfsnodes, rfn, link);
     }
     rfn->mark = 0;
--- src/fileaccess/fa_ntfs.c
+++ src/fileaccess/fa_ntfs.c
@@ -354,7 +354,7 @@ ntfs_periodic(struct callout *c, void *opaque)
       snprintf(url, sizeof(url), "%s://", d->mounts[j].name);
       d->services[j] = service_create_managed(url, d->mounts[j].name,
 					      url, "usb", NULL, 0, 1, 
-					      SVC_ORIGIN_MEDIA, 1,
+					      SVC_ORIGIN_MEDIA,
 					      "External NTFS volume");
     }
   }
