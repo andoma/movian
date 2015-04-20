44f5adcd4438f31404100de33b15de384d7b7b46
Andreas Öman <andreas@lonelycoder.com>
2015-04-20 07:02:13 +0200

    Add plugin example for async page loads

---
+++ plugin_examples/async_page_load/async_page_load.js
@@ -0,0 +1,24 @@
+var page = require('showtime/page');
+
+
+new page.Route('asyncPageLoad:test:(.*)', function(page, arg1) {
+  var offset = 0;
+
+  function loader() {
+    setTimeout(function() {
+
+      for(var i = 0; i < 20; i++) {
+        page.appendItem('asyncPageLoad:item:' + (offset + i), "directory", {
+          title: "Item" + (offset + i)
+        });
+      }
+      offset += 20;
+      page.haveMore(true);
+    }, 1000);
+
+  }
+
+  page.type = "directory";
+  page.asyncPaginator = loader;
+  loader();
+});
