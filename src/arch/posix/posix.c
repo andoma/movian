14369a257a690e20342e6f557d157a583dbf2940
Andreas Öman <andreas@lonelycoder.com>
2015-06-12 13:43:25 +0200

    posix: Print time when logging to console

--- src/arch/posix/posix.c
+++ src/arch/posix/posix.c
@@ -203,7 +203,19 @@ trace_arch(int level, const char *prefix, const char *str)
     sgroff = "\033[0m";
   }
 
-  fprintf(stderr, "%s%s %s%s\n", sgr, prefix, str, sgroff);
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+
+  struct tm tm;
+  localtime_r(&tv.tv_sec, &tm);
+
+
+  fprintf(stderr, "%s%02d:%02d:%02d.%03d: %s %s%s\n", sgr,
+          tm.tm_hour,
+          tm.tm_min,
+          tm.tm_sec,
+          (int)tv.tv_usec / 1000,
+          prefix, str, sgroff);
 
   if(gconf.trace_to_syslog)
     syslog(prio, "%s %s", prefix, str);
