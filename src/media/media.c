5cfc212864c0125c84079985dbdc029366dd7b90
Andreas Öman <andreas@lonelycoder.com>
2015-06-27 12:43:42 +0200

    media: Avoid calling prop subsystem from mp_release as it might deadlock
    Fixes #2734

--- src/media/media.c
+++ src/media/media.c
@@ -33,6 +33,7 @@
 #include "backend/backend.h"
 #include "settings.h"
 #include "misc/minmax.h"
+#include "task.h"
 
 #include "subtitles/video_overlay.h"
 #include "subtitles/dvdspu.h"
@@ -346,9 +347,20 @@ mp_destroy(media_pipe_t *mp)
 
 }
 
-
 /**
  *
+ */
+static void
+mp_final_release(void *aux)
+{
+  media_pipe_t *mp = aux;
+  prop_destroy(mp->mp_prop_root);
+  free(mp);
+}
+
+
+/**
+ * prop_mutex can be held here, so we need to avoid locking it
  */
 void
 mp_release(media_pipe_t *mp)
@@ -379,7 +391,6 @@ mp_release(media_pipe_t *mp)
   mq_destroy(&mp->mp_video);
 
 
-  prop_destroy(mp->mp_prop_root);
 
   video_overlay_flush_locked(mp, 0);
   dvdspu_destroy_all(mp);
@@ -395,7 +406,12 @@ mp_release(media_pipe_t *mp)
     atomic_dec(&media_buffer_hungry);
 
   cancellable_release(mp->mp_cancellable);
-  free(mp);
+
+  /**
+   * We need to destroy mp_prop_root but there is a risk that prop_mutex
+   * is held here, so we need to dispatch
+   */
+  task_run(mp_final_release, mp);
 }
 
 
