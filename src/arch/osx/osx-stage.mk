ebdf82991dec43019dc831b66481c1c0f4811904
Andreas Öman <andreas@lonelycoder.com>
2015-07-01 08:04:28 +0200

    osx: Fix build break

--- src/arch/osx/osx-stage.mk
+++ src/arch/osx/osx-stage.mk
@@ -2,7 +2,6 @@ ${APPDIR}/Contents/MacOS/showtime: ${APPPROG} \
 			${APPDIR}/Contents/Info.plist \
 			${APPDIR}/Contents/Resources/hts.icns \
 			${APPDIR}/Contents/Resources/MainMenu.nib \
-			${SPOTIFY_INST_FILES}
 	@echo "APP\t$@"
 	@mkdir -p $(dir $@)
 	@cp $< $@
@@ -14,8 +13,3 @@ ${APPDIR}/Contents/%.nib: support/Showtime.app/Contents/%.xib
 ${APPDIR}/Contents/%: support/Showtime.app/Contents/%
 	@mkdir -p $(dir $@)
 	@cp $< $@
-
-$(SPOTIFY_INST_PATH)/%: ${SPOTIFY_FRAMEWORK}/%
-	@mkdir -p $(dir $@)
-	@rm -f $@
-	@cp -a $< $@
