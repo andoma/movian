ea3037ec58eeaf0d399320c60dd3edc0a7e1a17f
Andreas Öman <andreas@lonelycoder.com>
2015-09-02 23:06:15 +0200

    Add PS3 support to new duk_config.h

--- ext/duktape/duk_config.h
+++ ext/duktape/duk_config.h
@@ -551,6 +551,17 @@ static __inline__ unsigned long long duk_rdtsc(void) {
 #include <sys/param.h>
 #include <sys/time.h>
 #include <time.h>
+#elif defined(__PPU__)
+/* PS3 PPU */
+#define DUK_USE_DATE_NOW_GETTIMEOFDAY
+#define DUK_USE_DATE_TZO_GMTIME_R
+#define DUK_USE_DATE_PRS_STRPTIME
+#define DUK_USE_DATE_FMT_STRFTIME
+#include <sys/types.h>
+#include <limits.h>
+#include <sys/param.h>
+#include <sys/time.h>
+#include <time.h>
 #else
 /* Other UNIX, hopefully others */
 #define DUK_USE_DATE_NOW_GETTIMEOFDAY
