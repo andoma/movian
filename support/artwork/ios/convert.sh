e9e29d4da0da542f332197a8f56608018f81e3e7
Andreas Smas <andreas@lonelycoder.com>
2016-05-31 20:22:34 +0200

    Artwork

--- support/artwork/ios/convert.sh
+++ support/artwork/ios/convert.sh
@@ -1,17 +1,15 @@
-SRC=origin.png
+SRC=../movian_1024_1024.png
 
 rm -rf out
 mkdir -p out
-convert origin.png -resize 29x29 out/Icon-Small.png
-convert origin.png -resize 58x58 out/Icon-Small@2x.png
-convert origin.png -resize 87x87 out/Icon-Small@3x.png
-convert origin.png -resize 40x40 out/Icon-40.png
-convert origin.png -resize 80x80 out/Icon-40@2x.png
-convert origin.png -resize 120x120 out/Icon-40@3x.png
-convert origin.png -resize 120x120 out/Icon-60@2x.png
-convert origin.png -resize 160x160 out/Icon-60@3x.png
-convert origin.png -resize 76x76 out/Icon-76.png
-convert origin.png -resize 152x152 out/Icon-76@2x.png
-convert origin.png -resize 167x167 out/Icon-83_5@2x.png
-
-
+convert ${SRC} -resize 29x29 out/Icon-Small.png
+convert ${SRC} -resize 58x58 out/Icon-Small@2x.png
+convert ${SRC} -resize 87x87 out/Icon-Small@3x.png
+convert ${SRC} -resize 40x40 out/Icon-40.png
+convert ${SRC} -resize 80x80 out/Icon-40@2x.png
+convert ${SRC} -resize 120x120 out/Icon-40@3x.png
+convert ${SRC} -resize 120x120 out/Icon-60@2x.png
+convert ${SRC} -resize 160x160 out/Icon-60@3x.png
+convert ${SRC} -resize 76x76 out/Icon-76.png
+convert ${SRC} -resize 152x152 out/Icon-76@2x.png
+convert ${SRC} -resize 167x167 out/Icon-83_5@2x.png
