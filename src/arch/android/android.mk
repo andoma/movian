b747c54aa12324b6b16408d9e03d90aead39d950
Andreas Smas <andreas@lonelycoder.com>
2018-03-18 17:05:16 -0700

    Build android for x86

--- src/arch/android/android.mk
+++ src/arch/android/android.mk
@@ -31,7 +31,7 @@ DX        := ${ANDROID_BUILD_TOOLS}/dx
 ZIPALIGN  := ${ANDROID_BUILD_TOOLS}/zipalign
 APKSIGNER := ${ANDROID_BUILD_TOOLS}/apksigner
 
-NUMVER := $(shell echo ${VERSION} | awk -F. '{ print $$3 + $$2 * 100000 + $$1 * 10000000 }')
+NUMVER := $(shell echo ${VERSION} | awk -F. '{ print $$3 + $$2 * 100000 + $$1 * 10000000 + $(ANDROID_BUILDKIND_DESCRIMINATOR) * 100000000}')
 
 R_JAVA := ${BUILDDIR}/java/com/lonelycoder/mediaplayer/R.java
 
@@ -40,7 +40,7 @@ JAVA_SRCS := $(shell find android/src/com/lonelycoder/mediaplayer -name '*.java'
 RESFILES := $(shell find android/res -type f)
 
 
-${BUILDDIR}/apk/lib/armeabi/libcore.so: ${LIB}.so
+${BUILDDIR}/apk/lib/${ANDROID_ABI}/libcore.so: ${LIB}.so
 	@mkdir -p $(dir $@)
 	${STRIP} -o $@ $<
 
@@ -51,38 +51,37 @@ ${BUILDDIR}/inst/lib/libavresample.so: $(BUILDDIR)/stamps/libav.stamp
 ${BUILDDIR}/inst/lib/libavutil.so:     $(BUILDDIR)/stamps/libav.stamp
 ${BUILDDIR}/inst/lib/libswscale.so:    $(BUILDDIR)/stamps/libav.stamp
 
-${BUILDDIR}/apk/lib/armeabi/%.so: ${BUILDDIR}/inst/lib/%.so
+${BUILDDIR}/apk/lib/${ANDROID_ABI}/%.so: ${BUILDDIR}/inst/lib/%.so
 	@mkdir -p $(dir $@)
 	${STRIP} -o $@ $<
 
 ${MANIFEST}: android/AndroidManifest.xml.in ${BUILDDIR}/version_git.h
-	sed >$@ -e s/@@VERSION@@/${VERSION}/g -e s/@@APPNAME@@/${APPNAMEUSER}/g -e s/@@VERCODE@@/${NUMVER}/g $<
-
+	sed >$@ -e s/@@VERSION@@/${VERSION}/g -e s/@@APPNAME@@/${APPNAMEUSER}/g -e s/@@VERCODE@@/${NUMVER}/g -e s/@@ANDROID_MIN_SDK_VERSION@@/${ANDROID_MIN_SDK_VERSION}/g -e s/@@ANDROID_TARGET_SDK_VERSION@@/${ANDROID_TARGET_SDK_VERSION}/g $<
 
 ${R_JAVA}: ${MANIFEST} ${RESFILES}
 	@mkdir -p ${BUILDDIR}/java
 	${AAPT} package -f -m -J ${BUILDDIR}/java -S android/res \
-	-M ${MANIFEST} -I ${ANDROID_PLATFORM}/android.jar
+	-M ${MANIFEST} -I ${ANDROID_PLATFORM_PATH}/android.jar
 
 ${BUILDDIR}/apk/classes.dex: ${JAVA_SRCS} ${R_JAVA}
 	@mkdir -p ${BUILDDIR}/classes
 	@mkdir -p $(dir $@)
 	javac -source 1.7 -target 1.7 \
 	-bootclasspath "${JAVA_HOME}/jre/lib/rt.jar" \
-	-classpath ${ANDROID_PLATFORM}/android.jar -d ${BUILDDIR}/classes \
+	-classpath ${ANDROID_PLATFORM_PATH}/android.jar -d ${BUILDDIR}/classes \
 	${R_JAVA} ${JAVA_SRCS}
 	${DX} --dex --output=$@ ${BUILDDIR}/classes
 
 ${BUILDDIR}/${APPNAME}.unsigned.apk: ${BUILDDIR}/apk/classes.dex ${RESFILES} \
-	${BUILDDIR}/apk/lib/armeabi/libcore.so \
-	${BUILDDIR}/apk/lib/armeabi/libavcodec.so \
-	${BUILDDIR}/apk/lib/armeabi/libavdevice.so \
-	${BUILDDIR}/apk/lib/armeabi/libavformat.so \
-	${BUILDDIR}/apk/lib/armeabi/libavresample.so \
-	${BUILDDIR}/apk/lib/armeabi/libavutil.so \
-	${BUILDDIR}/apk/lib/armeabi/libswscale.so
+	${BUILDDIR}/apk/lib/${ANDROID_ABI}/libcore.so \
+	${BUILDDIR}/apk/lib/${ANDROID_ABI}/libavcodec.so \
+	${BUILDDIR}/apk/lib/${ANDROID_ABI}/libavdevice.so \
+	${BUILDDIR}/apk/lib/${ANDROID_ABI}/libavformat.so \
+	${BUILDDIR}/apk/lib/${ANDROID_ABI}/libavresample.so \
+	${BUILDDIR}/apk/lib/${ANDROID_ABI}/libavutil.so \
+	${BUILDDIR}/apk/lib/${ANDROID_ABI}/libswscale.so
 	${AAPT} package -f -M ${MANIFEST} -S android/res \
-	-I ${ANDROID_PLATFORM}/android.jar -F $@ ${BUILDDIR}/apk/
+	-I ${ANDROID_PLATFORM_PATH}/android.jar -F $@ ${BUILDDIR}/apk/
 
 ${BUILDDIR}/${APPNAME}.aligned.apk: ${BUILDDIR}/${APPNAME}.unsigned.apk
 	${ZIPALIGN} -f -p 4 $< $@
@@ -90,9 +89,8 @@ ${BUILDDIR}/${APPNAME}.aligned.apk: ${BUILDDIR}/${APPNAME}.unsigned.apk
 ${BUILDDIR}/${APPNAME}.apk: ${BUILDDIR}/${APPNAME}.aligned.apk
 	${APKSIGNER} sign -ks android/movian.keystore -ks-pass env:MOVIAN_KEYSTORE_PASS --out $@ $<
 
-
 signed-apk: ${BUILDDIR}/${APPNAME}.apk
-	@echo "doozer-artifact:${BUILDDIR}/${APPNAME}.apk:apk:application/vnd.android.package-archive:${APPNAME}.apk:versioncode=${NUMVER}"
+	@echo "doozer-artifact:${BUILDDIR}/${APPNAME}.apk:apk:application/vnd.android.package-archive:${APPNAME}_api${ANDROID_MIN_SDK_VERSION}_${ANDROID_ABI}.apk:versioncode=${NUMVER}"
 
 install: ${BUILDDIR}/${APPNAME}.apk
 	adb install -r $<
